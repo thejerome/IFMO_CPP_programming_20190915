68c0c760dd9fc09bc5f1843269f46e86f0871881
alexmeh <alex.meh96@mail.ru>
2019-09-19 01:38:41 +0300

    t05

--- src/main/cpp/t05_chess_king.cpp
+++ src/main/cpp/t05_chess_king.cpp
@@ -16,9 +16,20 @@
 
 #include "t05_chess_king.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int t05_chess_king() {
+    int x1=0;
+    int y1=0;
+    int x2=0;
+    int y2=0;
 
+    cin >> x1 >>y1 >> x2 >> y2;
+
+    if ( abs(x1-x2)<=1 && abs(y1-y2)<=1 )
+        cout<<"YES";
+    else
+        cout<<"NO";
 };
