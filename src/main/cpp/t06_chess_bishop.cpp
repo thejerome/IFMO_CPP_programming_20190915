7168aba09773b23074067143b39a075cfd9cd76e
alexmeh <alex.meh96@mail.ru>
2019-09-19 01:38:59 +0300

    t06

--- src/main/cpp/t06_chess_bishop.cpp
+++ src/main/cpp/t06_chess_bishop.cpp
@@ -16,9 +16,20 @@
 
 #include "t06_chess_bishop.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int t06_chess_bishop() {
+    int x1=0;
+    int y1=0;
+    int x2=0;
+    int y2=0;
 
+    cin >> x1 >>y1 >> x2 >> y2;
+
+    if (abs(x1-x2) == abs(y1-y2))
+        cout<<"YES";
+    else
+        cout<<"NO";
 };
