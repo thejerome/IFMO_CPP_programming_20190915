863f25a8d6252722c1c5c4afdacd761b50d2b4c3
Yasumitai <55378459+Yasumitai@users.noreply.github.com>
2019-09-26 16:37:33 +0300

    Update t08_chess_knight.cpp

--- src/main/cpp/t08_chess_knight.cpp
+++ src/main/cpp/t08_chess_knight.cpp
@@ -29,5 +29,15 @@
 using namespace std;
 
 int t08_chess_knight() {
-
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if ((((c-a ==1) || (c-a == -1)) && ((d-b ==2) || (d-b == -2))) || (((c-a ==2) || (c-a == -2)) && ((d-b ==1) || (d-b == -1))))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+  return 0;
 };
