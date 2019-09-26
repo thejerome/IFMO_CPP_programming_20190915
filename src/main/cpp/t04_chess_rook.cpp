a366ee2aaa313b1eeeafc7d8745e5f8ea3d3371e
Yasumitai <55378459+Yasumitai@users.noreply.github.com>
2019-09-26 16:07:23 +0300

    Update t04_chess_rook.cpp

--- src/main/cpp/t04_chess_rook.cpp
+++ src/main/cpp/t04_chess_rook.cpp
@@ -21,7 +21,7 @@ using namespace std;
 int t04_chess_rook() {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    if ((a=c) || (b==d))
+    if ((a==c) || (b==d))
     {
         cout << "YES";
     }
