918e6810adc9601d81fbeaa227623472430f0096
datbeohbbh <phongtomfapp@gmail.com>
2019-09-29 03:29:14 +0300

    Update t08_chess.cpp

--- src/main/cpp/t08_chess.cpp
+++ src/main/cpp/t08_chess.cpp
@@ -18,12 +18,26 @@
 //   6   0   7   0   8
 
 #include "t08_chess.h"
-#include <iostream>
-
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int t08_chess() {
     
-    return 0;
+const int N = 102;
+int a[N][N];
+int n,m;
+    	cin>>n>>m;
+    	int cur = 0;
+    	for(int i = 1;i <= n;++i){
+    		for(int j = 1 + ((i & 1) ^ 1);j <= m;j += 2){
+    			a[i][j] = ++cur;
+    		}
+    	}
+    	for(int i = 1;i <= n;++i){
+    		for(int j = 1;j <= m;++j){
+    			cout<<setw(4)<<a[i][j];
+    		}
+    		cout<<'\n';
+    	}
 }
