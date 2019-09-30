7db7d5ada62b7d5ccf661a85b8f47af9af985d91
elruz1ano <elruz1ano@yandex.ru>
2019-09-30 08:53:37 +0300

    fixed 10sort

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,43 @@
 using namespace std;
 
 int t10_sort3() {
-
-};
+    int a = 0, b = 0, c = 0, max = 0, mid = 0, min = 0;
+    cin >> a >> b >> c;
+    if(a>b)
+    {
+        max = a;
+        mid = b;
+        min = c;
+    }
+    else
+    {
+        max = b;
+        mid = a;
+        min = c;
+    }
+    if (c>max && max == a)
+    {
+        max = c;
+        mid = a;
+        min = b;
+    }
+    else if (c > max && max == b)
+    {
+        max = c;
+        mid = b;
+        min = a;
+    }
+    else if (c>b && max ==a)
+    {
+        mid = c;
+        min = b;
+    }
+    else if (c>a && max == b)
+    {
+        mid = c;
+        min = a;
+    }
+    cout << min << mid << max << endl;
+    
+    return 0;
+}
