b4c3a83391417ac2362cb098ddeb8897f3387e07
Marygith <50752864+Marygith@users.noreply.github.com>
2019-09-30 09:02:13 +0300

    fixed max3

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -17,6 +17,21 @@
 
 using namespace std;
 
-int t01_max3() {
+int t01_max3() { 
+  
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a > b)
+  {
+  if (a > c) {cout << a;}
+    
+  else {cout << b;}
+  }
+  else
+  {
+  if (b > c) {cout << b;} 
+  else { cout << c;}
+  }
+   
 
 };
