922894d81cd3b8f67e213f2abcbc0eaae0020bb7
Shket228 <daniildudov@gmail.com>
2019-10-02 00:06:54 +0300

    a

---
+++ src/main/cpp/t03_twos.cpp
@@ -0,0 +1,34 @@
+//Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае.
+//
+//Формат входных данных
+//        Вводится натуральное число.
+//Формат выходных данных
+//        Выведите ответ на задачу.
+//Sample Input 1:
+//
+//1
+//Sample Output 1:
+//
+//YES
+//        Sample Input 2:
+//
+//2
+//Sample Output 2:
+//
+//YES
+
+#include "t03_twos.h"
+#include <iostream>
+
+using namespace std;
+
+int t03_twos() {
+    int i = 1, n = 0;
+    cin >> n;
+    while (i < n)
+        i *= 2;
+    if (i == n)
+        cout << "YES";
+    else
+        cout << "NO";
+};
---
+++ src/main/cpp/t08_fibb.cpp
@@ -0,0 +1,34 @@
+//Последовательность Фибоначчи определяется так:
+//
+//F(0) = 0, F(1) = 1, …, F(n) = F(n−1) + F(n−2).
+//
+//По данному числу N определите N-е число Фибоначчи F(N).
+//
+//Формат входных данных
+//        Вводится натуральное число N.
+//Формат выходных данных
+//        Выведите ответ на задачу.
+//Sample Input:
+//
+//6
+//Sample Output:
+//
+//8
+
+#include "t08_fibb.h"
+#include <iostream>
+
+using namespace std;
+
+int t08_fibb() {
+    int n = 0, pre = 0, cur = 1;
+    cin >> n;
+    if (n) {
+        while (--n) {
+            cur += pre;
+            pre = cur - pre;
+        }
+        cout << cur;
+    } else
+        cout << pre;
+};
