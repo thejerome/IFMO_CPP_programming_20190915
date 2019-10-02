c1d87b80009b7563ac950fbe48d140d31bf87e96
DyAvr <55398221+DyAvr@users.noreply.github.com>
2019-10-02 21:35:48 +0300

    Fix 1.1

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -23,16 +23,16 @@ using namespace std;
 
 int t06_quad() {
     int a,b,c;
-    double D,x1,x2;
+    double D,x1,x_2;
     cin >> a >> b >> c;
 
     D=b*b-4*a*c;
     if (D>0) {
         x1=(-b+sqrt(D))/(2*a);
-        x2=(-b-sqrt(D))/(2*a);
+        x_2=(-b-sqrt(D))/(2*a);
         if (x1>x2) {
-            cout << setprecision(6) << x2 << " " << x1;
-        } else cout << x1 << " " << x2;
+            cout << setprecision(6) << x_2 << " " << x1;
+        } else cout << x1 << " " << x_2;
     } else {
         if (D==0) {
             cout << -b/2/a;
