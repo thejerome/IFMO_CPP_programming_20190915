4d3e4d030eb48792a2d77c84ad14f8074f23930d
TalyonLoce <55375067+TalyonLoce@users.noreply.github.com>
2019-10-02 20:38:45 +0300

    Update t10_max_local.cpp

--- src/main/cpp/t10_max_local.cpp
+++ src/main/cpp/t10_max_local.cpp
@@ -24,5 +24,16 @@
 using namespace std;
 
 int t10_max_local() {
-
+  
+    int a, b, c, cnt = 0;
+    cin >> a >> b >> c;
+    while (c != 0) {
+        if ((b > a) and (b > c))
+            cnt ++;
+        a = b;
+        b = c;
+        cin >> c;
+    }
+    cout << cnt;
+  
 };
