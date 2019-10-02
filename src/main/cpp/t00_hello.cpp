fa5ac7ad518cb8ac107567d5843a0bccfef5da5d
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-10-02 03:39:54 +0300

    Update t00_hello.cpp

--- src/main/cpp/t00_hello.cpp
+++ src/main/cpp/t00_hello.cpp
@@ -13,13 +13,7 @@ using namespace std;
 
 int t00_hello() {
 
-int a,b;
 
-cout << "Enter two numbers :\n";
-cin >> a >> b;
+ cout <<"Hello, World!";
 
-cout <<  "addition : " << a + b << "\n";
-cout << "subtraction : " << a - b << "\n";
-cout << "multiplication : " << a * b << "\n";
-cout << "division : " << a / b << "\n";
 };
