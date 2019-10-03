266e517d4ab79526c4f74b5fa8cb1c21cd1da8d1
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-10-03 20:55:10 +0300

    Update t01_squirrels.cpp

--- src/main/cpp/t01_squirrels.cpp
+++ src/main/cpp/t01_squirrels.cpp
@@ -25,8 +25,7 @@ using namespace std;
 
 int t01_squirrels() {
    int k,n;  //k = nuts and n = squirrels
-    cout<<"Enter number of nuts and number of squirrels : " << endl;
 	cin>>n>>k;
-	cout<<"Every squirrel will get : " << k/n;
+	cout<< k/n;
 	return 0;
 };
