a4ec3743f2223fe545931c9a716987a1067f874d
deadinsidezxc <lordsymon99@mail.ru>
2019-10-03 17:49:55 +0300

    roflan zdarova

--- src/main/cpp/t05_tens_number.cpp
+++ src/main/cpp/t05_tens_number.cpp
@@ -25,6 +25,6 @@ using namespace std;
 int t05_tens_number() {
     int N;
     cin >> N;
-    cout << (N % 10 % 10);
+    cout << (N % 100 - N % 10);
     return 0;
 };
--- src/main/cpp/t06_3digits.cpp
+++ src/main/cpp/t06_3digits.cpp
@@ -25,6 +25,6 @@ using namespace std;
 int t06_3digits() {
     int N;
     cin >> N;
-    cout << (N % 10)+(N % 10 % 10)+(N % 10 % 10 % 10);
+    cout << (N / 100)+(N % 100 / 10)+(N  % 10);
     return 0;
 };
