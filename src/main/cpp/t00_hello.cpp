0e720d9de24a98a6502c2521b67519042d443005
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-03 18:13:58 +0300

    Update t00

--- src/main/cpp/t00_hello.cpp
+++ src/main/cpp/t00_hello.cpp
@@ -1,16 +1,8 @@
-//Напишите программу, выводящую "Hello, World!". Можно скопировать ее из конспекта.
-//
-//Sample Input:
-//
-//Sample Output:
-//
-//Hello, World!
-
-#include "t00_hello.h"
 #include <iostream>
 
 using namespace std;
 
-int t00_hello() {
-
-};
+int main(){
+    cout << "Hello_World";
+    return 0;
+}
