23f4b8e1c3d36b18cdb32058c6ea964fc29562bd
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-04 22:58:27 +0300

    Update t_08

--- src/main/cpp/t08_watch.cpp
+++ src/main/cpp/t08_watch.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     int a,b,c;
    cin >> c;
-   b = (c / 60) %60 ;
-   a = (c /3600) %24 ;
+   b = (c / 60) % 60 ;
+   a = (c /3600) % 24 ;
    c = c % 60 ;
    printf("%02d:%02d:%02d", a, b, c);
     return 0;
