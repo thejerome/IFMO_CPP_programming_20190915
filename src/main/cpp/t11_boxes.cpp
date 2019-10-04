3f0d10476fb82c81f0b65afe76e22c83463f8b16
Mark <m.gurianov@mail.ru>
2019-10-04 10:56:37 +0300

    *

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -78,4 +78,8 @@ int t11_boxes() {
     
     if ((a1 == a2) && (b1 == b2) && (c1 == c2)) cout << "Boxes are equal";
     else if ((a1 <= a2) && (b1 <= b2) && (c1 <= c2))
+        cout <<"The first box is smaller than the second one";
+    else if ((a1 >= a2) && (b1 >= b2) && (c1 >= c2))
+                            cout <<"The first box is larger than the second one";
+    else cout <<"Boxes are incomparable";
 };
