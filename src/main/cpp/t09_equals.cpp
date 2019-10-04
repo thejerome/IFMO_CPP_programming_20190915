069633cbc34cde0697de925a289fcccaa6a8eaeb
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-10-04 21:30:56 +0300

    Update t09_equals.cpp

--- src/main/cpp/t09_equals.cpp
+++ src/main/cpp/t09_equals.cpp
@@ -21,10 +21,24 @@
 
 #include "t09_equals.h"
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
 int t09_equals() {
-
+  int i, j, n, count = 0; 
+    cin >> n;
+    vector <int> a(n);
+    for ( i = 0; i < n; i++) {
+     cin >> a[i];   
+    }
+    for ( i = 0; i < n-1; i++) {
+        for ( j = i+1; j < n; j++) {
+            if ( a[i] == a[j] ) {
+                count++;
+            }
+        }
+    }
+    cout << count;
+  return 0;
 }
