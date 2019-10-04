ec02a557ff2a2d991e734e2a3cb4c083e8714ab5
malkyatmuk1 <tanya.naidenova@abv.bg>
2019-10-04 19:00:06 -0400

    change = and <>

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -45,8 +45,10 @@ int t11_boxes() {
     if(a1>b1) {swap=b;b=a;a=swap;}
     if(b1>c1) {swap=c;c=b;b=swap;}
     if(a1>b1) {swap=b;b=a;a=swap;}
-    if(a>a1 && b>b1 && c>c1) cout<<"The first box is larger than the second one";
-    else if(a<a1 && b<b1 && c<c1) cout<<"The first box is smaller than the second one";
-    else if(a==a1 && b==b1 && c==c1) cout<<"Boxes are equal";
+   
+    if(a==a1 && b==b1 && c==c1) cout<<"Boxes are equal";
+    else if(a>=a1 && b>=b1 && c>=c1) cout<<"The first box is larger than the second one";
+    else if(a=<a1 && b=<b1 && c=<c1) cout<<"The first box is smaller than the second one";
+    
     else cout<<"Boxes are incomparable";
 };
