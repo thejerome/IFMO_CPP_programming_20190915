b1c7d5c60593c9d520c1d614173ec6fd96461a98
Nin <nin@MacBook-Pro-Nin.local>
2019-10-06 15:58:23 +0300

    task 1

--- src/main/cpp/t09_snail.cpp
+++ src/main/cpp/t09_snail.cpp
@@ -27,5 +27,12 @@
 using namespace std;
 
 int t09_snail() {
+    int H,A,B;
+    cin >> H;
+    cin >> A;
+    cin >> B;
+    cout << (H-B)/(A-B)+((H-B)%(A-B)+(A-B)-1)/(A-B);
 
-};
+    return 0;
+
+}
