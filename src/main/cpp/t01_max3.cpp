c25eac31d3f657372ae18ceff5b4bd2e4da65be4
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-10-06 16:10:18 +0300

    Update t01_max3.cpp

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -18,5 +18,10 @@
 using namespace std;
 
 int t01_max3() {
-
+  int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= b && a >= c) cout << a << endl;
+    else if (b >= c) cout << b << endl;
+    else cout << c << endl;
+    return 0;
 };
