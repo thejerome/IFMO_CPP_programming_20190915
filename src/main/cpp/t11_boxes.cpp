d9a267ce5cfeca3e878ec6d4273f3c536302e9bb
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-10-06 23:49:19 +0300

    Update t11_boxes.cpp

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -39,11 +39,11 @@ using namespace std;
 int t11_boxes() {
 
 
-  int A1, B1, C1, A2, B2, C2;
+    int A1, B1, C1, A2, B2, C2;
     cin >> A1 >> B1 >> C1 >> A2 >> B2 >> C2;
 
 
-    if ((A1 * B1 * C1 < A2 * B2 * C2))
+    if ((A1 * B1 * C1 < A2 * B2 * C2) | (A1 + B1 + C1 < A2+B2+C2))
     {
         cout << "The first box is smaller than the second one";
 
@@ -67,6 +67,7 @@ int t11_boxes() {
     {
         cout << "Boxes are incomparable";
     }
+
 
 
 
