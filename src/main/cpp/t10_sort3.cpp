b0409e77a5063e1a7cb8d97e9ace4fc7f0b4573e
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-10-06 04:04:32 +0300

    Update t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -23,24 +23,24 @@ int A,B,C = 0;
 cin >> A >> B >> C;
 
 if (((A >= B) && (B >= C)) )
-    cout << A << " " << B << " " << C;
+    cout << C << " " << B << " " << A;
 else if (((A >= C) && (C >= B)))
-
-    cout << A << " " << C << " " << B;
-
-else if (((B >= A) && (A >= C)) )
-
-    cout << B << " " << A << " " << C;
-
-else if (((B >= C) && (C >= A)))
 
     cout << B << " " << C << " " << A;
 
-else if (((C >= A) && (A >= B)))
+else if (((B >= A) && (A >= C)) )
 
     cout << C << " " << A << " " << B;
 
+else if (((B >= C) && (C >= A)))
+
+    cout << A << " " << C << " " << B;
+
+else if (((C >= A) && (A >= B)))
+
+    cout << B << " " << A << " " << C;
+
 else if (((C >= B) && (B >= A)))
 
-    cout << C << " " << B << " " << A;
+    cout << A << " " << B << " " << C;
 };
