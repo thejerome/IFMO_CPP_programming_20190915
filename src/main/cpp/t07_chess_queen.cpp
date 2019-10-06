38cf458bba6dcc7c95e62cd6408ebc993e89feba
afruza <56043168+afruza@users.noreply.github.com>
2019-10-06 23:55:19 +0500

    Update t07_chess_queen.cpp
    23:59

--- src/main/cpp/t07_chess_queen.cpp
+++ src/main/cpp/t07_chess_queen.cpp
@@ -29,5 +29,12 @@
 using namespace std;
 
 int t07_chess_queen() {
-
+  int x1,y1,x2,y2;
+  cin  >> y1 >> x1 >> y2 >> x2;
+    if (abs(x1-x2) == abs(y1-y2) || ((y1==y2 &&  x1!=x2) || (x1==x2 &&  y1!=y2))) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+return 0;
 };
