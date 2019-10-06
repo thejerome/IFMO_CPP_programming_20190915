3fbf629799058a131ad6a551397f96a909474bb4
afruza <56043168+afruza@users.noreply.github.com>
2019-10-07 01:10:28 +0500

    Update t10_sort3.cpp
    1:14

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,35 @@
 using namespace std;
 
 int t10_sort3() {
+  int a, b, c;
+    cin >> a >> b >> c;
+    if ( a <= b && a <= c ) {
+     if ( c <= b) {
+         cout << a << " " << c << " " << b;
+     }
+        else {
+        cout << a << " " << b << " " << c;     
+        }
+    }
+    else {
+     if ( b <= a && b <= c) {
+      if ( c <= a ) {
+      cout << b << " " << c << " " << a;  
+      }
+         else {
+          cout << b << " " << a << " " << c;   
+         }
+     }
+        else {
+         if ( a<=b ) {
+          cout << c << " " << a << " " << b;   
+         }
+            else {
+            cout << c << " " << b << " " << a;    
+            }
+        }
 
+    }
+  return 0;
 };
+
