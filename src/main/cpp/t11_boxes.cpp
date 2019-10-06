6ba5e500999277fad35ec7771a0ae9fb45162cab
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-10-06 23:32:40 +0300

    Update t11_boxes.cpp

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -38,15 +38,12 @@ using namespace std;
 
 int t11_boxes() {
 
-  
-    int A1, B1, C1, A2, B2, C2;
+ int A1, B1, C1, A2, B2, C2;
     cin >> A1 >> B1 >> C1 >> A2 >> B2 >> C2;
 
-       if (((A1 > A2) && (A1 > B2) && (A1 > C2) || ((B1 > A2) && (B1 > B2) && (B1 > C2)) || ((C1 > A2) && (C1 > B2) && (C1 > C2)) || ((A2 > A1) && (A2 > B1) && (A2 > C1)) || ((B2 > A1) && (B2 > B1) && (B2 > C1)) || ((C2 > A1) && (C2 > B1) && (C2 > C1)))&& (A1 * B1 * C1 !=  A2 * B2 * C2))
-    {
-        cout << "Boxes are incomparable";
-    }
-    else if (A1 * B1 * C1 < A2 * B2 * C2)
+   
+
+    if ((A1 * B1 * C1 < A2 * B2 * C2) && (((A1 < A2) && (B1 < B2) && (C1 < C2)) || ((A1 < C2) && (B1 < B2) && (C1 < A2))))
     {
         cout << "The first box is smaller than the second one";
 
@@ -65,6 +62,9 @@ int t11_boxes() {
         cout << "Boxes are equal";
     }
 
-
+    else ((A1*B1*C1 != A2 *B2*C2) && (A1+B1+C1 != A2 +B2+C2))
+    {
+        cout << "Boxes are incomparable";
+    }
 
 };
