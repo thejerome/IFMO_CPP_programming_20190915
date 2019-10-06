3658a41cb617b4a50813b80ae8aab4c78edc6d1b
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-10-06 04:00:27 +0300

    Update t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,28 @@
 using namespace std;
 
 int t10_sort3() {
+int A,B,C = 0;
+cin >> A >> B >> C;
 
+if (((A >= B) && (B >= C)) )
+    cout << A << " " << B << " " << C;
+else if (((A >= C) && (C >= B)))
+
+    cout << A << " " << C << " " << B;
+
+else if (((B >= A) && (A >= C)) )
+
+    cout << B << " " << A << " " << C;
+
+else if (((B >= C) && (C >= A)))
+
+    cout << B << " " << C << " " << A;
+
+else if (((C >= A) && (A >= B)))
+
+    cout << C << " " << A << " " << B;
+
+else if (((C >= B) && (B >= A)))
+
+    cout << C << " " << B << " " << A;
 };
