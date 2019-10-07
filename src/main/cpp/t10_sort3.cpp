a4df336b445513b119fa28cadfc584472c06e97e
Laertire <55375793+Laertire@users.noreply.github.com>
2019-10-07 22:23:15 +0900

    [10]

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,37 @@
 using namespace std;
 
 int t10_sort3() {
-
+int a,b,c = 0;
+cin >> a >> b >> c;
+if(a >= b){
+	if(b >= c){
+		cout << c << " " << b << " " << a;
+		return 0;
+	}
+	if(a >= c){
+		cout << b << " " << c << " " << a;
+		return 0;
+	}
+	else{
+		cout << b << " " << a << " " << c;
+		return 0;
+	}
+}
+else{
+	if(b >= c){
+		if(c >= a){
+			cout << a << " " << c << " " << b;
+			return 0;
+		}
+		else {
+			cout << c << " " << a << " " << b;
+			return 0;
+		}
+	}
+	else {
+		cout << a << " " << b << " " << c;
+		return 0;
+	}
+}
+return 0;
 };
