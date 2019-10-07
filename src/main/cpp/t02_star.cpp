9298157652ec023f6f5693e06cc0db7115cb89d3
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-10-08 01:25:24 +0300

    Update t02_star.cpp

--- src/main/cpp/t02_star.cpp
+++ src/main/cpp/t02_star.cpp
@@ -24,6 +24,24 @@
 using namespace std;
 
 int t02_star() {
-
+  int n, i, j;
+    cin >> n;
+    string a[n][n];
+    for (i = 0; i < n; i++) {
+        for ( j = 0; j < n; j++) {
+            if ( i == n / 2 || j == n / 2 || i == j  || i+j == n-1) {
+                a[i][j] = "*";
+            }
+                else {
+                    a[i][j] = ".";
+                }
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for ( j = 0; j < n; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
