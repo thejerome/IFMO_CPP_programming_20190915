b42b0ffcd912d2dc82686d84b111d5ef73819653
RED-1917 <55047602+RED-1917@users.noreply.github.com>
2019-10-07 18:14:16 +0300

    Update t01_max3.cpp

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -18,5 +18,21 @@
 using namespace std;
 
 int t01_max3() {
-
+int a,b,c;
+ cin>>a;
+ cin>>b;
+ cin>>c;
+ if(a>b && a>c)
+ {
+     cout<<a<<endl;
+ }
+ if(b>a && b>c)
+ {
+     cout<<b<<endl;
+ }
+ if(c>a && c>b)
+ {
+     cout<<c<<endl;
+ }
+ return 0;
 };
