365f26c5d1821237185e2f556a70c4d0be42e7d2
Laertire <55375793+Laertire@users.noreply.github.com>
2019-10-09 04:44:40 +0900

    [11] fix 4

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -39,8 +39,8 @@ using namespace std;
 int t11_boxes() {
 int a,b,c,aa,bb,cc = 0;
 	cin >> a >> b >> c >> aa >> bb >> cc;
-    if((a >= aa & ((b >= bb & c >= cc) || (b >= cc & c >= bb))) || (a >= bb & ((b >= aa & c >= cc) || (b >= cc & c >= aa))) || (a >= cc & ((b >= aa & c >= bb) || (b >= bb & c >= aa))) ){
-    	if ((a == aa & ((b == bb & c == cc) || (b == cc & c == bb))) || (a == bb & ((b == aa & c == cc) || (b == cc & c == aa))) || (a == cc & ((b == aa & c == bb) || (b == bb & c == aa)))){
+    if((a >= aa and ((b >= bb and c >= cc) or (b >= cc and c >= bb))) or (a >= bb and ((b >= aa and c >= cc) or (b >= cc and c >= aa))) or (a >= cc and ((b >= aa and c >= bb) or (b >= bb and c >= aa))) ){
+    	if ((a == aa and ((b == bb and c == cc) or (b == cc and c == bb))) or (a == bb and ((b == aa and c == cc) or (b == cc and c == aa))) or (a == cc and ((b == aa and c == bb) or (b == bb and c == aa)))){
     		cout << "Boxes are equal";
     		return 0;
     	}
@@ -49,8 +49,8 @@ int a,b,c,aa,bb,cc = 0;
     		return 0;
     	}
     }
-    if((aa >= a & ((bb >= b & cc >= c) || (bb >= c & cc >= b))) || (aa >= b & ((bb >= a & cc >= c) || (bb >= c & cc >= a))) || (aa >= c & ((bb >= a & cc >= b) || (bb >= b & cc >= a))) ){
-    	if (a == aa & b == bb & c == cc){
+    if((aa >= a and ((bb >= b and cc >= c) or (bb >= c and cc >= b))) or (aa >= b and ((bb >= a and cc >= c) or (bb >= c and cc >= a))) or (aa >= c and ((bb >= a and cc >= b) or (bb >= b and cc >= a))) ){
+    	if (a == aa and b == bb and c == cc){
     		cout << "Boxes are equal";
     		return 0;
     	}
