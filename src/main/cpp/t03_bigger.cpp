1047bd2ab383796bb6ac097153d307734afc7da7
CatArthur <mssizov@gmail.com>
2019-10-10 20:25:54 +0300

    redone

--- src/main/cpp/t03_bigger.cpp
+++ src/main/cpp/t03_bigger.cpp
@@ -22,7 +22,8 @@ int t03_bigger() {
     int n=0;
     cin>>n;
     int a[n];
-    for(int i=0;i<n;i++){
+    cin>>a[0];
+    for(int i=1;i<n;i++){
         cin>>a[i];
         if(a[i]>a[i-1])
             cout<<a[i]<<" ";
