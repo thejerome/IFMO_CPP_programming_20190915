e0f94ea2920ee38f9b7f6eb3cab13044582c83a2
Marygith <50752864+Marygith@users.noreply.github.com>
2019-10-11 23:14:25 +0300

    fixed bishop

--- src/main/cpp/t06_chess_bishop.cpp
+++ src/main/cpp/t06_chess_bishop.cpp
@@ -22,6 +22,6 @@ using namespace std;
 int t06_chess_bishop() {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-    if (a/c == b/d or a+b == c+d) {cout << "YES";}
+    if (a-c == b-d) {cout << "YES";}
     else {cout <<"NO";}
 }
