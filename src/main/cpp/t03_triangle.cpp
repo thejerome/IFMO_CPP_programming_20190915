be588320a33787a179dd1b6f6ec7f99c84679cc7
Mimmey <motyolya@gmail.com>
2019-10-12 21:15:50 +0500

    Done

--- src/main/cpp/t03_triangle.cpp
+++ src/main/cpp/t03_triangle.cpp
@@ -20,5 +20,14 @@
 using namespace std;
 
 int t03_triangle() {
+	float a, b, c, s, p;
 
+    cin >> a >> b >> c;
+
+    p = (a + b + c) / 2;
+    s = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    cout << setprecision(4) << fixed << s << endl;
+
+    return 0;
 };
