de92e79e3d5e6b04b5ce0416e770da83d46ca499
Sasha Mikhailova <55459169+MikhSaas@users.noreply.github.com>
2019-10-13 19:53:09 +0300

    Update t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,34 @@
 using namespace std;
 
 int t10_sort3() {
+    int m,n,k;
+    cin>>m;
+    cin>>n;
+    cin>>k;
+if (m>=n && m>=k) {
+    if (k>=n) {
+        cout<<n<<' '<<k<<' '<<m;
+    }
+    else {
+        cout<<k<<' '<<n<<' '<<m;
+    }
+}
+if (n>m && n>k) {
+    if (k>m) {
+        cout<<m<<' '<<k<<' '<<n;
+    }
+    else {
+        cout<<k<<' '<<m<<' '<<n;
+    }
+}
+if (k>m && k>n) {
+    if (n>m) {
+        cout<<m<<' '<<n<<' '<<k;
+    }
+    else {
+        cout<<n<<' '<<m<<' '<<k;
+    }
+}
 
-};
+    return 0;
+}
