a69de575350502326f573d876c73ab93f8564a0b
afruza <56043168+afruza@users.noreply.github.com>
2019-10-13 20:35:49 +0500

    Update t06_quad.cpp

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -26,8 +26,8 @@ int t06_quad() {
     d = pow ( b , 2 ) - 4 * a * c;
     if ( d > 0 ) {
         x1 = ( b * (-1) -  sqrt(d) ) / 2 * a;
-        x2 = ( b * (-1) + sqrt(d) ) / 2 * a;
-    }
+        x2 = ( b * (-1) + sqrt(d) ) / 2 * a
+         
         if ( x1 > x2 ) {
             cout << x2 << " " << x1; 
         }
@@ -35,7 +35,7 @@ int t06_quad() {
             cout << x1 << " " << x2;
         }
     } 
-    else if  ( d == 0 ) {
+      if  ( d == 0 ) {
      cout << ( b * (-1) ) / 2 * a; 
     }
         
