4ead3a54b5fcc0f6a1e0754a0980f20d62750537
afruza <56043168+afruza@users.noreply.github.com>
2019-10-13 20:55:33 +0500

    Update t06_quad.cpp

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -22,7 +22,7 @@ using namespace std;
 
 int t06_quad() {
 double a, b, c, d, x1, x2;
-    cin >> a >> b >> c
+    cin >> a >> b >> c;
     d =  pow ( b , 2 ) - 4 * a * c;
     if ( d > 0 ) {
      x1 = ( b * (-1) + sqrt(d) ) / 2 * a; 
