0709b4a45f987e7686651f1a156f4572a2d67579
TalyonLoce <55375067+TalyonLoce@users.noreply.github.com>
2019-10-14 23:15:48 +0300

    Update t10_unique.cpp

--- src/main/cpp/t10_unique.cpp
+++ src/main/cpp/t10_unique.cpp
@@ -25,5 +25,22 @@
 using namespace std;
 
 int t10_unique() {
+    int n;
+    cin >> n;
+    int a[n];
+    bool f;
+    for (int i = 0; i < n; i ++)
+        cin >> a[i];
+
+    for (int i = 0; i < n - 1; i ++) {
+        f = true;
+        for (int j = 0; j < n; j ++)
+            if (a[i] == a[j] and i != j) {
+                f = false;
+                break;
+            }
+        if (f)
+            cout << a[i] << " ";
+    }
 
 }
