a73d806acd46e65202ee003aaf6c4fa3b202e5e0
Anatoly122 <tmail2020@mail.ru>
2019-10-14 13:07:10 +0300

    modified:   src/main/cpp/t01_frac.cpp 	modified:   src/main/cpp/t02_frac_2.cpp 	modified:   src/main/cpp/t03_triangle.cpp 	modified:   src/main/cpp/t04_deposit.cpp 	modified:   src/main/cpp/t05_ave.cpp 	modified:   src/main/cpp/t06_quad.cpp 	modified:   src/main/cpp/t07_quad2.cpp

--- src/main/cpp/t04_deposit.cpp
+++ src/main/cpp/t04_deposit.cpp
@@ -22,5 +22,16 @@
 using namespace std;
 
 int t04_deposit() {
-
+	double P = 0, X = 0, Y = 0, K = 0, J = 0;
+	int i = 0;
+	cin >> P >> X >> Y >> K;
+	while (i < K) 
+	{
+		i++;
+		J = X;
+		X = int(X * (1 + P / 100)) + int((Y * (1 + P / 100))/100);
+		Y = int(Y * (1 + P / 100)) % 100 + (J * (1 + P / 100)-int(J * (1 + P / 100)))*100;
+		if (Y >= 100) { X = X + 1; Y = Y - 100; }
+	}
+	cout << X << " " << Y;
 };
