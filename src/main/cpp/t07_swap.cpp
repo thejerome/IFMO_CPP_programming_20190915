f3e218bb50efa143e7fbdcaa0704a987be0b8d88
DyAvr <55398221+DyAvr@users.noreply.github.com>
2019-10-14 11:15:51 +0300

    Edit task-5

--- src/main/cpp/t07_swap.cpp
+++ src/main/cpp/t07_swap.cpp
@@ -19,5 +19,18 @@
 using namespace std;
 
 int t07_swap() {
-
+    int n,t;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < (n/2*2); i=i+2) {
+        t=a[i];
+        a[i]=a[i+1];
+        a[i+1]=t;
+    }
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << ' ';
+    }
 }
--- src/main/cpp/t08_cycleswap.cpp
+++ src/main/cpp/t08_cycleswap.cpp
@@ -19,5 +19,20 @@
 using namespace std;
 
 int t08_cycleswap() {
-
+    int n,t,bef;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    bef=a[0];
+    for (int i = 1; i < n; i++) {
+        t=a[i];
+        a[i]=bef;
+        bef=t;
+    }
+    a[0]=bef;
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
 }
