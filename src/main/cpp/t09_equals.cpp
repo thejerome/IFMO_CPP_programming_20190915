80ff803ad21695b1f866f07b476f9a4864f18be7
TalyonLoce <55375067+TalyonLoce@users.noreply.github.com>
2019-10-14 23:10:11 +0300

    Update t09_equals.cpp

--- src/main/cpp/t09_equals.cpp
+++ src/main/cpp/t09_equals.cpp
@@ -27,4 +27,16 @@ using namespace std;
 
 int t09_equals() {
 
+    int n;
+    cin >> n;
+    int a[n], cnt = 0;
+    for (int i = 0; i < n; i ++)
+        cin >> a[i];
+
+    for (int i = 0; i < n - 1; i ++)
+        for (int j = i + 1; j < n; j ++)
+            if (a[i] == a[j])
+                cnt += 1;
+    cout << cnt;
+
 }
