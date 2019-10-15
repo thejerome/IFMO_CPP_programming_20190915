d9526b96141f12134430566a7608a0ade4bbc866
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-10-15 17:02:19 +0300

    fix

--- src/main/cpp/t04_sum.cpp
+++ src/main/cpp/t04_sum.cpp
@@ -20,12 +20,13 @@
 using namespace std;
 
 int t04_sum() {
-  int N = 1, sum;
+    int N = 1;
+    int sum;
 
     while (N != 0) {
-     cin >> N;
-     sum += N;
-}
+        cin >> N;
+        sum += N;
+    }
     cout << sum << endl;
     return 0;
 };
