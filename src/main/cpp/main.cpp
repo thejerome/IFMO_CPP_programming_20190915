1ba02e2ff9c23f0964d746383f8af8fda5ea2628
datbeohbbh <phongtomfapp@gmail.com>
2019-10-15 21:42:51 +0300

    solve task-6

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -2,18 +2,6 @@
 #include <map>
 #include <functional>
 #include <cstring>
-<<<<<<< HEAD
-#include "t00_hello.h"
-#include "t01_squirrels.h"
-#include "t02_squirrels2.h"
-#include "t03_last_number.h"
-#include "t04_first_number.h"
-#include "t05_tens_number.h"
-#include "t06_3digits.h"
-#include "t07_pies.h"
-#include "t08_watch.h"
-#include "t09_snail.h"
-=======
 #include "t01_max.h"
 #include "t02_star.h"
 #include "t03_diag.h"
@@ -23,7 +11,6 @@
 #include "t07_snake.h"
 #include "t08_chess.h"
 #include "t09_spiral.h"
->>>>>>> 918e6810adc9601d81fbeaa227623472430f0096
 
 using namespace std;
 
@@ -36,9 +23,6 @@ int main(int argc, char **argv) {
 
     std::map <std::string, std::function<int()>> funcMap =
             {
-<<<<<<< HEAD
-                    {"t00_hello",        t00_hello}
-=======
                     {"t01_max",    t01_max},
                     {"t02_star",   t02_star},
                     {"t03_diag",   t03_diag},
@@ -49,7 +33,6 @@ int main(int argc, char **argv) {
                     {"t08_chess",  t08_chess},
                     {"t09_spiral", t09_spiral}
 
->>>>>>> 918e6810adc9601d81fbeaa227623472430f0096
             };
 
     funcMap[functionName]();
