1b1ab8c634eccb48d3ef4553eed74a7bfe4025da
TalyonLoce <55375067+TalyonLoce@users.noreply.github.com>
2019-10-15 22:23:28 +0300

    Update t03_circle.cpp

--- src/main/cpp/t03_circle.cpp
+++ src/main/cpp/t03_circle.cpp
@@ -38,8 +38,18 @@
 using namespace std;
 
 //function IsPointInCircle
+bool IsPointInCircle(double x, double y, double xc, double yc, double r) {
+    return sqrt((xc - x) * (xc - x) + (yc - y) * (yc - y)) <= r;
+}
 
 int t03_circle() {
+
+    double x, y, xc, yc, r;
+    cin >> x >> y >> xc >> yc >> r;
+    if (IsPointInCircle(x, y, xc, yc, r))
+        cout << "YES";
+    else
+        cout << "NO";
 
     return 0;
 }
