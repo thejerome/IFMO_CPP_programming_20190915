8180a18155eafa4d7b28c03916fd1f7d9bb18e59
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-10-16 15:57:48 +0300

    Update t06_quad.cpp

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -25,8 +25,8 @@ int t06_quad() {
     cin >> a >> b >> c;
     d = b * b - 4 * a * c;
     if ( d > 0 ) {
-     x1 = ((-1) * b + sqrt(d)) / 2 / a; 
-     x2 = ((-1) * b - sqrt(d)) / 2 / a;  
+     x1 = (-b + sqrt(d)) / 2 / a; 
+     x2 = (-b - sqrt(d)) / 2 / a;  
         if ( x1 < x2 ) {
          cout << x1 << " " << x2;
         }
@@ -35,7 +35,7 @@ int t06_quad() {
         }
     }
     if ( d == 0 ) {
-       x1 = ((-1) * b + sqrt(d)) / 2 / a; 
+       x1 = (-sqrt(d)) / 2 / a; 
         cout << x1;
     }
   return 0;
