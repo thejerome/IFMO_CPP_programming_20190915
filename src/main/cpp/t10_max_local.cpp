ff4151f5278dec6d8d40059cf4de03d7ee696eba
Marygith <50752864+Marygith@users.noreply.github.com>
2019-10-17 00:03:21 +0300

    fixed max local

--- src/main/cpp/t10_max_local.cpp
+++ src/main/cpp/t10_max_local.cpp
@@ -24,12 +24,14 @@
 using namespace std;
 
 int t10_max_local() {
+
     int s, n , k = 0, c = 0;
     cin >> s;
     cin >> n;
     while (n!=0) {
-        if (c > n) { k++; }
+        if (c > n) { k++; c = 0;}
         if (n > s) { c = n;}
+
         s = n;
         cin >> n;
     }
