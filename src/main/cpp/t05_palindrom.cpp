a1c76e1b4fe4b8bcbd11dce4a7d1a077a70c3617
TalyonLoce <55375067+TalyonLoce@users.noreply.github.com>
2019-10-16 17:55:10 +0300

    Update t05_palindrom.cpp

--- src/main/cpp/t05_palindrom.cpp
+++ src/main/cpp/t05_palindrom.cpp
@@ -18,10 +18,22 @@
 
 #include "t05_palindrom.h"
 #include <iostream>
+#include <cstring>
 
 
 using namespace std;
 
 int t05_palindrom() {
+  
+    char c[523];
+    cin.getline(c, 523);
+    for (int i = 0; i < strlen(c) / 2; i ++)
+        if (c[i] != c[strlen(c) - i - 1]) {
+            cout << "no";
+            c[0] = '#';
+            break;
+        }
+    if (c[0] != '#')
+        cout << "yes";
 
 }
