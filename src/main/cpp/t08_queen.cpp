9299825fcbb60fa18bfbfd8ea6da915c4122b18e
TalyonLoce <55375067+TalyonLoce@users.noreply.github.com>
2019-10-16 03:41:00 +0300

    Update t08_queen.cpp

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -22,7 +22,36 @@
 
 using namespace std;
 
-int t08_queen(){
+bool isCapture(int x1, int y1, int x2, int y2) {
+    return ((x1 - x2 == y1 - y2) or (x1 - x2 == y2 - y1) or (y1 == y2));
+}
 
+int queen(int n, int c, int a[], int cnt) {
+    if (c < n * 2) {
+        int j;
+        for (int i = 0; i < n * 2; i += 2){
+            a[c + 1] = i / 2;
+            j = c - 2;
+            for (j; j > -1; j -= 2)
+                if (isCapture(a[c], a[c + 1], a[j], a[j + 1]))
+                    break;
+            if (j == -2) {
+                cnt = queen(n, c + 2, a, cnt);
+            }
+        }
+    } else
+        cnt += 1;
+    return cnt;
+}
+
+int t08_queen(){
+    
+    int n;
+    cin >> n;
+    int a[n * 2];
+    for (int i = 0; i < n; i ++)
+        a[i * 2] = i;
+    cout << queen(n, 0, a, 0);
+    
     return 0;
 }
