d39cdfacdcd5961289bea6cd76d17203df9619f7
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-17 18:30:43 +0300

    Update t11

--- src/main/cpp/t11_boxes.cpp
+++ src/main/cpp/t11_boxes.cpp
@@ -39,18 +39,28 @@ using namespace std;
 int t11_boxes() {
   int a, b, c, d, e, f;
     cin >> a >> b >> c >> d >> e >> f;
-    if ((a*b*c)>(d*e*f)) {
-        cout << "The first box is larger than the second one";
+    if ((a >= d and ((b >= e and c >= f) or (b >= f and c >= e))) or (a >= e and ((b >= d and c >= f) or (b >= f and c >= d))) or (a >= f and ((b >= d and c >= e) or (b >= e and c >= d))) ) {
+        if ((a == d and ((b == e and c == f) or (b == f and c == e))) or (a == e and ((b == d and c == f) or (b == f and c == d))) or (a == f and ((b == d and c == e) or (b == e and c == d)))) {
+            cout << "Boxes are equal";
+            return 0;
+        } else {
+            cout << "The first box is larger than the second one";
+            return 0;
+        }
+    }
+    if((d >= a and ((e >= b and f >= c) or (e >= c and f >= b))) or (d >= b and ((e >= a and f >= c) or (e >= c and f >= a))) or (d >= c and ((e >= a and f >= b) or (e >= b and f >= a))) ){
+        if ((a == d and ((b == e and c == f) or (b == f and c == e))) or (a == e and ((b == d and c == f) or (b == f and c == d))) or (a == f and ((b == d and c == e) or (b == e and c == d)))){
+            cout << "Boxes are equal";
+            return 0;
+        }
+        else {
+            cout << "The first box is smaller than the second one";
+            return 0;
+        }
+    }
+    else {
+        cout << "Boxes are incomparable";
         return 0;
     }
-    if ((a*b*c)<(d*e*f)) {
-        cout << "The first box is smaller than the second one";
-        return 0;
-    }
-    if ((a*b*c)==(d*e*f)) {
-        cout << "Boxes are equal";
-        return 0;
-    }
-    cout << "Boxes are incomparable";
-    return 0;
+}
 };
