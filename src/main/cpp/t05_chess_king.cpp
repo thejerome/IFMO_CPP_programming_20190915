17425c7f0cd081e9d76bf30d57c146fe44eae4b2
lenix161 <lenyasklyarov@gmail.com>
2019-10-17 22:15:57 +0300

    reshil

--- src/main/cpp/t05_chess_king.cpp
+++ src/main/cpp/t05_chess_king.cpp
@@ -20,5 +20,11 @@
 using namespace std;
 
 int t05_chess_king() {
-
-};
+	int x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	if(abs(x1-x2)<=1 && abs(y1-y2)<=1)
+		cout << "YES";
+	else
+		cout << "NO";
+return 0;
+}
--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -20,4 +20,19 @@ using namespace std;
 
 int t10_sort3() {
 
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a >= b && b >= c)
+		cout << c << " " << b << " " << a;
+    else if(a >= c && c >= b)
+    	cout << b << " " << c << " " << a;
+    else if(b >= c && c >= a)
+    	cout << a << " " << c << " " << b;
+    else if(b >= a && a >= c)
+    	cout << c << " " << a << " " << b;
+    else if(c >= b && b >= a)
+    	cout << a << " " << b << " " << c;
+    else if (c >=a && a >=b) 
+    	cout << b << " " << a << " " << c;
+return 0;
 };
