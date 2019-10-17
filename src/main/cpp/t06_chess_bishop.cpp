5d1b8a2a2e2f8dc8909b90c529773c4dee99bf26
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-17 17:09:19 +0300

    Update t06

--- src/main/cpp/t06_chess_bishop.cpp
+++ src/main/cpp/t06_chess_bishop.cpp
@@ -24,9 +24,11 @@ int t06_chess_bishop() {
     cin >> a >> b >> c >> d ;
     if (a!=c and b!=d) {
         if (((a+b)%2)==((c+d)%2)) {
+          if ((a/c)==(b/d) or (a/c) == (d/b)) { 
             cout << "YES";
             return 0;
         }
+      }    
     }
     cout << "NO";
     return 0;
