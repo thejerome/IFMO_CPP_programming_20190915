3a9a8a333151cacb98edb4620b2d6a79b43425d3
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-17 16:16:26 +0300

    Update t07

--- src/main/cpp/t07_chess_queen.cpp
+++ src/main/cpp/t07_chess_queen.cpp
@@ -29,5 +29,19 @@
 using namespace std;
 
 int t07_chess_queen() {
+  int a,b,c,d ;
+    cin >> a >> b >> c >> d ;
+    if (a!=c & b!=d) {
+        if (((a+b)%2)==((c+d)%2)) {
+            cout << "YES";
+            return 0;
+        }
+    }
+    if (a==c | b==d) {
+        cout << "YES";
+        return 0;
+    }
+    cout << "NO";
+    return 0;
 
 };
