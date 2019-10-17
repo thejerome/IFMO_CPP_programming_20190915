3b3f83468881fafcbf40cb1a49e5db76f0224d8b
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-17 17:03:35 +0300

    Update t02

--- src/main/cpp/t02_triangle.cpp
+++ src/main/cpp/t02_triangle.cpp
@@ -21,19 +21,19 @@ using namespace std;
 int t02_triangle() {
   int a , b , c  ;
     cin >> a >> b >> c;
-    if (a>b and a>c) {
+    if (a>=b and a>=c) {
         if (a<(b+c)) {
             cout << "YES";
             return 0;
     }
     }
-    if (b>a and b>c) {
+    if (b>=a and b>=c) {
         if (b<(a+c)) {
             cout << "YES";
             return 0;
         }
     }
-    if (c>a and c>b) {
+    if (c>=a and c>=b) {
         if (c<(a+b)) {
             cout << "YES";
             return 0;
