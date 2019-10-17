0601b9aeb283a81db12f89eccffbcefec14ec68e
Marygith <50752864+Marygith@users.noreply.github.com>
2019-10-17 21:13:08 +0300

    fixed quad2

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -20,14 +20,15 @@
 
 using namespace std;
 
-int t07_quad2() {
     float a, b, c, d, x1, x2;
     cin >> a >> b >> c;
     d = b*b - 4*a*c;
-    if (d >= 0 and a!=0) {
+    if (d >= 0 and a!=0 ) {
         x1 = (-b + sqrt(d))/2/a;
         x2 = (-b - sqrt(d))/2/a;
-        if(x1 == x2) {cout << 1 << " " << " " << x1;} else {if(x1 > x2) {cout << 2 << " " << x2 << " " << x1;} else {cout << 2 << x1 << " " << x2;}}}
+        if(x1 == x2) {cout << 1 << " " << x1;} else {if(x1 > x2) {cout << 2 << " " << x2 << " " << x1;} else {cout << 2 << x1 << " " << x2;}}}
     if(d < 0) {cout << 0;}
-    if(a == 0) {cout << 1 << -c/b;}
+    if(a == 0 and b!= 0) {cout << 1 << " " << -c/b;}
+    if(a == 0 and b == 0 and c!=0) {cout << 0;}
+    if(a == 0 and b == 0 and c==0) {cout << 3;}
 };
