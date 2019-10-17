3bd425474307e996480e256a255fa12c7d585638
Sasha Mikhailova <55459169+MikhSaas@users.noreply.github.com>
2019-10-17 18:58:07 +0300

    Update t01_even.cpp

--- src/main/cpp/t01_even.cpp
+++ src/main/cpp/t01_even.cpp
@@ -19,5 +19,17 @@
 using namespace std;
 
 int t01_even() {
+    int n;
+    int a[n];
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    for (int i=0;i<n;i++) {
+        if (i%2==0) {
+            cout<<a[i]<<' ';
+        }
+    }
 
+    return 0;
 }
