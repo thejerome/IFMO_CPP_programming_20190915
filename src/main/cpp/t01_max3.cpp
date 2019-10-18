acfbaf7bc68e05b2f94307d478cbff5313252457
RED-1917 <55047602+RED-1917@users.noreply.github.com>
2019-10-19 01:16:28 +0300

    Update t01_max3.cpp

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -22,16 +22,13 @@ int a,b,c;
  cin>>a;
  cin>>b;
  cin>>c;
- if(a>b && b>c)
- {
+ if((a>=b && b>=c)||(a>=c && c>=b)){
      cout<<a<<endl;
  }
- if(b>a && a>c)
- {
+ else if((b>=a && a>=c)||(b>=c && c>=a)){
      cout<<b<<endl;
  }
- if(c>a && a>b)
- {
+ else if((c>=a && a>=b)||(c>=b && b>=a)){
      cout<<c<<endl;
  }
  return 0;
