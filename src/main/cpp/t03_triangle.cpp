d043dd89a2872381e61f3db68a80e384c8ec6987
Timophey Podolenchuk <p.timon17@gmail.com>
2019-10-18 20:16:44 +0300

    fix

--- src/main/cpp/t03_triangle.cpp
+++ src/main/cpp/t03_triangle.cpp
@@ -21,7 +21,7 @@ using namespace std;
 
 int t03_triangle() {
 
-    double a, b, c, p;
+    float a, b, c, p;
     cin >> a >> b >> c;
     p = (a + b + c) / 2;
     cout << sqrt(p *(p - a)*(p - b)*(p - c));
