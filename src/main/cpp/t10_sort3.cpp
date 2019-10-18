4f515fb3441a31fa2a067dbeb81e381bc35e467f
RED-1917 <55047602+RED-1917@users.noreply.github.com>
2019-10-18 23:55:54 +0300

    Update t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,27 @@
 using namespace std;
 
 int t10_sort3() {
-
+int a,b,c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if(a>=b && b>=c){
+        cout<<c<<" "<<b<<""<<a<<endl;
+    }
+    else if(a>=c && c>=b){
+        cout<<b<<" "<<c<<" "<<a<<endl;
+    }
+    else if(b>=a && a>=c){
+        cout<<c<<" "<<a<<" "<<b<<endl;
+    }
+    else if(b>=c && c>=a){
+        cout<<a<<" "<<c<<" "<<b<<endl;
+    }
+    else if(c>=a && a>=b){
+        cout<<b<<" "<<a<<" "<<c<<endl;
+    }
+    else if(c>=b && b>=a){
+        cout<<a<<" "<<b<<" "<<c<<endl;
+    }
+   return 0
 };
