1127ecf78dafb86b0d322374b6ad8f46cddce761
Timophey Podolenchuk <p.timon17@gmail.com>
2019-10-18 19:55:05 +0300

    task-4

--- src/main/cpp/t05_ave.cpp
+++ src/main/cpp/t05_ave.cpp
@@ -23,4 +23,15 @@ using namespace std;
 
 int t05_ave() {
 
+    double a, med = 0, i = 0;
+    while (a != 0) {
+        cin >> a;
+        if (a == 0) break;
+        i = i + 1;
+        med = med + a;
+    }
+    cout << fixed << setprecision(11);
+    cout << (double) med / i ;
+    return 0;
+
 };
--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -22,4 +22,41 @@ using namespace std;
 
 int t07_quad2() {
 
+    double a, b, c, x1, x2, dis, zero;
+    cout << setprecision(6) << fixed;
+    zero = 1.0e-7;
+    cin >> a >> b >> c;
+    if (fabs(a) <= zero && fabs(b) <= zero && fabs(c) <= zero){
+        cout << 3;
+    }
+    else if (not fabs(a) <= zero) {
+        dis = b * b - 4 * a * c;
+        if (dis > zero) { // есть 2 ответа
+            cout << 2 << " ";
+            x1 = (-b - sqrt(dis)) / (2 * a);
+            x2 = (-b + sqrt(dis)) / (2 * a);
+            if (x1 < x2) cout << x1 << " " << x2;
+            else cout << x2 << " " << x1;
+        }
+        else if (fabs(dis) <= zero) { // есть 1 ответ
+            cout << 1 << " ";
+            if (fabs(b) <= zero) cout << 0;
+            else {
+                x1 = -b / (2 * a);
+                cout << x1;
+            }
+        }
+        else cout << 0; // ответа нет
+    }
+    else if (fabs(b) <= zero) cout << 0; // ответа нет
+    else {
+        cout << 1 << " ";
+        if (fabs(c) <= zero) cout << 0; // ответа нет
+        else {
+            x1 = -c / b;
+            cout << x1;
+        }
+    }
+    return 0;
+
 };
