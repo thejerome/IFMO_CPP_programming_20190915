662096b799f7ec0f8764cd78def4f578488caa19
RED-1917 <55047602+RED-1917@users.noreply.github.com>
2019-10-19 22:28:25 +0300

    Update t04_deposit.cpp

--- src/main/cpp/t04_deposit.cpp
+++ src/main/cpp/t04_deposit.cpp
@@ -30,6 +30,6 @@ double P,X,Y,K,i;
         Y=floor(Y);
         i++;
     }
-    cout<<fixed<<setprecision(0)<<Y/100<<" "<<int(Y)%100<<endl;
+    cout<<Y/100<<" "<<int(Y)%100<<endl;
     return 0;
 };
