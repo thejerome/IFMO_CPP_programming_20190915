26c28dc0a18574a116a0dae1405734f39ee869c6
chromyy <55399522+chromyy@users.noreply.github.com>
2019-10-19 23:57:13 +0300

    Update t05_max.cpp

--- src/main/cpp/t05_max.cpp
+++ src/main/cpp/t05_max.cpp
@@ -20,5 +20,14 @@
 using namespace std;
 
 int t05_max() {
-
+    int a;
+    int max;
+    cin << a;
+    max = a;
+    while (a != 0){
+        cin >> a;
+        if (a > max){
+            max = a;
+        };
+    };
 };
