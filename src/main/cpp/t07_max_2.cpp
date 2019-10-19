e5b3f545d7fc62db472a387ad6dfaf88f57192f9
RED-1917 <55047602+RED-1917@users.noreply.github.com>
2019-10-19 19:03:30 +0300

    Update t07_max_2.cpp

--- src/main/cpp/t07_max_2.cpp
+++ src/main/cpp/t07_max_2.cpp
@@ -39,7 +39,7 @@ int a,max1,max2;
             max2=max1;
             max1=a;
         }
-        else if((a>max2 && a<max1)){
+        else if((a>max2 && a<=max1)){
             max2=a;
         }
     }
