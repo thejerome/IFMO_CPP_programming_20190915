319fa830ae64361b72c9515d6404fa18d1afbb8c
chromyy <55399522+chromyy@users.noreply.github.com>
2019-10-19 23:54:07 +0300

    Update t03_twos.cpp

--- src/main/cpp/t03_twos.cpp
+++ src/main/cpp/t03_twos.cpp
@@ -23,5 +23,18 @@
 using namespace std;
 
 int t03_twos() {
+    int n;
+    cin >> n;
+    while (n % 2 == 0){
+        n /= 2;
+    };
+
+    if (n == 1){
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    };
+    return 0;
 
 };
