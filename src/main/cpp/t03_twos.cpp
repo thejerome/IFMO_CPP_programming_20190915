bb52b957789e6f5280a696b06684acf6021c2041
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-10-20 13:28:26 +0300

    Update t03

--- src/main/cpp/t03_twos.cpp
+++ src/main/cpp/t03_twos.cpp
@@ -23,5 +23,21 @@
 using namespace std;
 
 int t03_twos() {
+    int a, b;
+    b = 2;
+    cin >> a ;
+    while (a>=b) {
+        if (a==b) {
+            cout << "YES";
+            return 0;
+        }
+        b= b*2;
+    }
+    if (a==1) {
+        cout << "YES";
+        return 0;
+    }
+    cout << "NO";
+    return 0;
 
 };
