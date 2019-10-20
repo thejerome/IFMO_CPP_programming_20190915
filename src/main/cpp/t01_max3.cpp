c4ec3d13fc7e979151a58b8615347c057d92352a
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-10-20 18:07:07 +0300

    Update t01_max3.cpp

--- src/main/cpp/t01_max3.cpp
+++ src/main/cpp/t01_max3.cpp
@@ -18,5 +18,10 @@
 using namespace std;
 
 int t01_max3() {
-
+int a,b,c;
+cin >> a >> b >> c;
+if((a>=b) && (a>=c)) cout << a<<"\n";
+else if(b>=c) cout<<b<<"n";
+else cout<<c<<"\n";
+return 0;
 };
