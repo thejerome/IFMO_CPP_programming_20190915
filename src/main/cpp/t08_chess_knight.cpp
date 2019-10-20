46c8bf53f80d2e5f0262bdf9a11a0dd55dc6fbf0
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-10-20 18:31:24 +0300

    Update t08_chess_knight.cpp

--- src/main/cpp/t08_chess_knight.cpp
+++ src/main/cpp/t08_chess_knight.cpp
@@ -29,5 +29,10 @@
 using namespace std;
 
 int t08_chess_knight() {
-
+int A1,B1,A2,B2;
+    cin >> A1 >> B1 >> A2 >> B2;
+    if (((A2 -  A1 == 1) || (A2 - A1 == -1)) || ((B2 - B1 == 2) || (B2 - B1 ==-2)))
+        cout << "YES";
+        else cout << "NO";
+        return 0;
 };
