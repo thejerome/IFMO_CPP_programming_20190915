bcdc7e013de770ce658931d1c07ae1fd63b6320a
chromyy <55399522+chromyy@users.noreply.github.com>
2019-10-21 01:49:46 +0300

    Update t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -19,5 +19,30 @@
 using namespace std;
 
 int t10_sort3() {
+    int a, b, c3, tempf,tempm,templ = 0;
+    cin >>a >>b >> c;
+    if (a>=b && a>=c) {
+        tempf = a;
+        tempm = b;
+        templ = c;
+    }
+    if (b>=a && b>=c) {
+        temp = b;
+        tempm = a;
+        templ = c;
+    }
+    if (c>=a && c>=b) {
+        temp = c;
+        tempm = a;
+        templ = b;
+    }
+    if (tempm>templ){
+        cout << templ << " " << tempm << " " << tempf;
+    }
+    else{
+        cout << tempm << " " << templ << " " << tempf;
+    };
+    return 0;
+
 
 };
