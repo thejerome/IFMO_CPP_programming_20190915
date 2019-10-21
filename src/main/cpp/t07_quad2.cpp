452941e6b9bbd4c8b46e24a5bdcf333ee89eb64d
maandronova <55444394+maandronova@users.noreply.github.com>
2019-10-22 02:03:16 +0300

    Update t07_quad2.cpp

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -29,13 +29,13 @@ int t07_quad2() {
         if (d > 0) {
             x1 = (((-b) - sqrt(d)) / (2 * a));
             x2 = (((-b) + sqrt(d)) / (2 * a));
-            if (x1 < x2) { cout << "2 " << x1 << " " << x2; } else { cout << "2 " << x2 << " " << x1; }
+            if (x1 < x2) { cout << 2 << " " << x1 << " " << x2; } else { cout << 2 << " " << x2 << " " << x1; }
         } else if (d == 0) {
             double x1 = (-b) / (2 * a);
-            cout << "1 " << x1;
+            cout << 1 << x1;
         } else cout << 0;
-    } else if ((a==0) && (b != 0)) cout << "1 " << (-c) / b;
-    else if ((a==0) && (c == 0) && (b != 0)) cout << "1 " << 0;
+    } else if ((a==0) && (b != 0)) cout << 1 << " " << (-c) / b;
+    else if ((a==0) && (c == 0) && (b != 0)) cout << 1 << " " << 0;
     else if ((a==0) && (c == 0) && (b = 0)) cout << 3;
     else if ((a==0) && (c != 0) && (b = 0)) cout << 0;
     return 0;
