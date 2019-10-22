fba6f6ca7258befd8bd524c7e91d421af0eba1c5
lexan4ello <koloskov747@gmail.com>
2019-10-23 00:51:30 +0300

    task-2 Колосков Алексей Юрьевич P3122

--- src/main/cpp/t06_chess_bishop.cpp
+++ src/main/cpp/t06_chess_bishop.cpp
@@ -16,14 +16,14 @@
 
 #include "t06_chess_bishop.h"
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int t06_chess_bishop() {
-    int x1 = 0 , x2 = 0 , y1 = 0 , y2 = 0 ;
-    cin >> x1 >> x2 >> y1 >> y2 ;
-    if (( y1 - x1) == ( y2 - x2 ))
- {
+    int x1 = 0 , y1 = 0 , x2 = 0 , y2 = 0 ;
+    cin >> x1 >> y1 >> x2 >> y2 ;
+    if (abs( x1 - x2) == abs( y1 - y2 ))
+    {
         cout << "YES";
     }
     else {
