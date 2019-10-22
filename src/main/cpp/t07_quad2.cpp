2c4825677694cc5694daba760266a25df1c311ea
maandronova <55444394+maandronova@users.noreply.github.com>
2019-10-22 11:51:30 +0300

    Update t07_quad2.cpp

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -23,20 +23,23 @@ using namespace std;
 int t07_quad2() {
     int a, b, c, d = 0;
     cin >> a >> b >> c;
-    if (a != 0) {
-        double x1, x2;
-        d = b * b + 4 * a * c;
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) cout << 3;
+            else cout << 0;
+        } else if (c == 0) cout << 1 << " " << 0;
+        else cout << 1 << " " << ((-c) / b);
+        else
+        d = b * b - 4 * a * c;
         if (d > 0) {
-            x1 = (((-b) - sqrt(d)) / (2 * a));
-            x2 = (((-b) + sqrt(d)) / (2 * a));
-            if (x1 < x2) { cout << 2 << " " << x1 << " " << x2; } else { cout << 2 << " " << x2 << " " << x1; }
+            double x1 = ((-b + sqrt(d)) / (2 * a));
+            double x2 = ((-b - sqrt(d)) / (2 * a));
+            if (x1 < x2) { cout << 2 << " " << x1 << " " << x2; } else { cout << 2 << " " << x2 << " " << x1 }
         } else if (d == 0) {
-            double x1 = (-b) / (2 * a);
-            cout << 1 << x1;
+            double x = (-b) / (2 * a);
+            cout << 1 << " " << x;
         } else cout << 0;
-    } else if ((a==0) && (b != 0)) cout << 1 << " " << (-c) / b;
-    else if ((a==0) && (c == 0) && (b != 0)) cout << 1 << " " << 0;
-    else if ((a==0) && (c == 0) && (b = 0)) cout << 3;
-    else if ((a==0) && (c != 0) && (b = 0)) cout << 0;
+    };
     return 0;
 }
+    
