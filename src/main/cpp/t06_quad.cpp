6823507e893036a636949e37b5b27ac8b3d4bb3e
Yasumitai <55378459+Yasumitai@users.noreply.github.com>
2019-10-24 23:28:36 +0300

    Update t06_quad.cpp

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -21,24 +21,21 @@
 using namespace std;
 
 int t06_quad() {
-    double a, b, c, D=0, x1, x2;
+    double a, b, c, D=0;
     cin >> a >> b >> c;
     D = (b*b) - (4*a*c);
     if (D == 0)
     {
-        x1 = (-b/(2*a));
-        cout << x1;
+        cout << (-b/(2*a));
     }
     if (D > 0)
     {
-        x1 = (-b + sqrt(D))/(2*a);
-        x2 = (-b - sqrt(D))/(2*a);
-        if (x1>x2)
+        if (((-b + sqrt(D))/(2*a)) > ((-b - sqrt(D))/(2*a)))
         {
-            cout << x2 << " " << x1 ;
+            cout << ((-b - sqrt(D))/(2*a)) << " " << ((-b + sqrt(D))/(2*a)) ;
         } else
         {
-            cout << x1 << " " << x2 ;
+            cout << ((-b + sqrt(D))/(2*a)) << " " << ((-b - sqrt(D))/(2*a)) ;
         }
     }
     return 0;
