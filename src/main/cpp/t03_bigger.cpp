04b418864898c62b80405669e0d7d31e946a0e59
Yurikyakyt <55650447+Yurikyakyt@users.noreply.github.com>
2019-10-24 09:36:28 +0300

    Update t03_bigger.cpp

--- src/main/cpp/t03_bigger.cpp
+++ src/main/cpp/t03_bigger.cpp
@@ -19,5 +19,13 @@
 using namespace std;
 
 int t03_bigger() {
-
+    int n;
+    cin >> n;
+    int a[n]{};
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        if ((a[i] > (a[i-1])&&(i>0))) {
+            cout << a[i] << ' ';
+        }
+    }
 }
