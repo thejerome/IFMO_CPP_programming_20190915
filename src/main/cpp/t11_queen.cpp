61fc16771f463f0028739780ebd63acfb4a32edc
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-10-29 20:43:09 +0300

    Update t11_queen.cpp

--- src/main/cpp/t11_queen.cpp
+++ src/main/cpp/t11_queen.cpp
@@ -39,5 +39,21 @@
 using namespace std;
 
 int t11_queen() {
+  int a[16];
+    int i = 0;
+    int j = 0;
+	for (i = 0; i < 16; i++){
+		cin >> a[i];
+	}
+	for (i = 0; i < 16; i += 2){
+		for (j = i + 2; j < 16; j += 2){
+			if (a[i] == a[j] || a[i + 1] == a[j + 1] || (abs(a[i] - a[j]) == abs(a[i + 1] - a[j + 1]))){
+				cout << "YES" << endl;
+				return 0;
 
+			}
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
 }
