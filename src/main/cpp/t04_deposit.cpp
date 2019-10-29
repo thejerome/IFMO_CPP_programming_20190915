9aeb909191c6f96a48a301400eb2484ad8562864
homa207 <homa207@mail.ru>
2019-10-29 22:36:24 +0300

    modified:   src/main/cpp/t04_deposit.cpp

--- src/main/cpp/t04_deposit.cpp
+++ src/main/cpp/t04_deposit.cpp
@@ -25,7 +25,8 @@ int t04_deposit() {
 	int p, x, y, n, i = 0;
 		cin >> p >> x >> y >> n;
 		while (i < n) {
-			
+			int z;
+			z = x * p % 100;
 			x = x + x * p / 100;
 			y = y + y * p / 100 + z;
 			x = x + y / 100;
