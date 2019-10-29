2a780fcc596ff968903a3bfa7df786637948aa22
sonyazel <55684759+sonyazel@users.noreply.github.com>
2019-10-29 22:19:44 +0300

    Update t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -20,28 +20,32 @@ using namespace std;
 
 int t10_sort3()
 {
-      int x1, x2, x3;
+     int x1, x2, x3;
     cin >> x1 >> x2 >> x3;
-       if ((x1 > x2)&&(x1 > x3))
+    if ((x1 >= x2)&&(x1 >= x3))
     {
-        if (x2 > x3)
+        if (x2 >= x3)
             cout << x3 << ' ' << x2 << ' ' << x1;
         else
             cout << x2 << ' ' << x3 << ' ' << x1;
     }
-     if ((x2 > x1)&&(x2 > x3))
-    {
-        if (x1 > x3)
-            cout << x3 << ' ' << x1 << ' ' << x2;
+    else
+        {
+        if ((x2 >= x1) && (x2 >= x3))
+        {
+            if (x1 >= x3)
+                cout << x3 << ' ' << x1 << ' ' << x2;
+            else
+                cout << x1 << ' ' << x3 << ' ' << x2;
+        }
         else
-            cout << x1 << ' ' << x3 << ' ' << x2;
-    }
-    if ((x3 > x1)&&(x3 > x2))
-    {
-        if (x2 > x1)
-            cout << x1 << ' ' << x2 << ' ' << x3;
-        else
-            cout << x2 << ' ' << x1 << ' ' << x3;
+        if ((x3 >= x1) && (x3 >= x2))
+        {
+            if (x2 >= x1)
+                cout << x1 << ' ' << x2 << ' ' << x3;
+            else
+                cout << x2 << ' ' << x1 << ' ' << x3;
+        }
     }
     return 0;
-    };
+};
