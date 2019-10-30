126cc157aeb61557c911afbda96564e9f9a17501
Yurikyakyt <55650447+Yurikyakyt@users.noreply.github.com>
2019-10-30 21:43:25 +0300

    Update t06_different.cpp

--- src/main/cpp/t06_different.cpp
+++ src/main/cpp/t06_different.cpp
@@ -19,5 +19,16 @@
 using namespace std;
 
 int t06_different() {
-
+    int n;
+    cin >> n;
+    int a[n]{};
+    cin >> a[0];
+    int m = 1;
+    for (int i=1;i<n;i++){
+        cin >> a[i];
+        if (a[i] != a[i-1]){
+            m++;
+        }
+    }
+    cout << m;
 }
