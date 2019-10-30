e4751e8828caeaed66a278afc585947449b9a0fb
Polina <55378557+chipa371@users.noreply.github.com>
2019-10-30 21:46:35 +0300

    Update t07_swap.cpp
    21:46

--- src/main/cpp/t07_swap.cpp
+++ src/main/cpp/t07_swap.cpp
@@ -19,5 +19,26 @@
 using namespace std;
 
 int t07_swap() {
-
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i = 0; i < n; i++){
+    cin >> a[i];
+  }
+  if(n % 2 == 0){
+    for(int i = 0; i < n-1; i+=2){
+      a[i] += a[i+1];
+      a[i+1] = a[i]-a[i+1];
+      a[i] -= a[i+1];
+    }
+  }
+  else{
+    for(int i = 0; i < n-2; i+=2){
+      a[i] += a[i+1];
+      a[i+1] = a[i]-a[i+1];
+      a[i] -= a[i+1];
+    }
+  }
+  for(int i = 0; i < n; i++)
+    cout << a[i];
 }
