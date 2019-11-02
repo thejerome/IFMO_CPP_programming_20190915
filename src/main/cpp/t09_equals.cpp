7001ee9cd62ae555c9fff7f892374dcd1f65f455
afruza <56043168+afruza@users.noreply.github.com>
2019-11-02 23:06:00 +0500

    Update t09_equals.cpp

--- src/main/cpp/t09_equals.cpp
+++ src/main/cpp/t09_equals.cpp
@@ -25,6 +25,18 @@
 
 using namespace std;
 
-int t09_equals() {
-
+int t09_equals() { 
+  int j,i,n,count=0;
+  cin >> n; 
+  int a[n];
+  for (int i=0;i<n;i++) {
+    cin >> a[i];  
 }
+for (i=0;i<n;i++) { 
+  for ( j=i+1;j<n;j++) {
+    if ( a[i]==a[j]) {
+      count++;
+    }
+  }
+}
+  cout << count;
