652fd113a53f02b93e6ff077c35ec66357c48e8f
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-04 00:06:54 +0300

    Update t04_neighbors.cpp

--- src/main/cpp/t04_neighbors.cpp
+++ src/main/cpp/t04_neighbors.cpp
@@ -21,19 +21,20 @@
 using namespace std;
 
 int t04_neighbors() {
-	int n;
-	cin >> n;
-	vector <int> a;
-	for (int i = 0; i < n; i++) {
-		int x;
-		cin >> x;
-		a.push_back(x);
-	}
-	for (int i = 1; i < n; i++) {
-		if (a[i] > 0 && a[i - 1] > 0 || a[i] < 0 && a[i - 1] < 0) {
-			cout << a[i - 1] << " " << a[i];
-			break;
-		}
-	}
-	return 0;
+  int n, i;
+cin >> n;
+int a[n];
+for(i = 0; i < n; i++)
+{
+cin >> a[i];
+}
+for(i = 1; i < n; i++){
+if((a[i] > 0 && a[i-1] > 0) || (a[i] < 0 && a[i - 1] < 0)){
+if (a[i] > a[i-1]){
+cout << a[i - 1] << " " << a[i];
+}else
+cout << a[i] << " " << a[i-1];
+}break;
+}
+return 0;
 }
