0a69ee5a6936d365f0d63c946a1c86281ee2e96f
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-04 01:27:52 +0300

    Update t10_unique.cpp

--- src/main/cpp/t10_unique.cpp
+++ src/main/cpp/t10_unique.cpp
@@ -25,35 +25,21 @@
 using namespace std;
 
 int t10_unique() {
-	int n, x, sum = 0;
-	cin >> n;
-	vector <int> a;
-	vector <int> counts;
-	vector <int> val;
-	for (int i = 0; i < n; i++) {
-		cin >> x;
-		a.push_back(x);
-	}
-	val.push_back(a[0]);
-	counts.push_back(1);
-
-	for (int i = 1; i < n; i++) {
-		bool added = false;
-		for (int j = 0; j < val.size(); j++) {
-			if (val[j] == a[i]) {
-				counts[j]++;
-				added = true;
-			}
-		}
-		if (not added) {
-			val.push_back(a[i]);
-			counts.push_back(1);
-		}
-	}
-	for (int j = 0; j < counts.size(); j++) {
-		if (counts[j] == 1) {
-			cout << val[j] << " ";
-		}
-	}
-	return 0;
+int x;
+cin>>x;
+int a[x];
+for(int i=0;i<x;i++){
+        cin>>a[i];}
+for(int i=0;i<x;i++){
+    int j,y=0;
+    for (j=0;j<x;j++){
+        if(a[i]-a[j]==0){
+            y++;
+        }
+    }
+    if (y-2<0){
+        cout<<a[i]<<" ";
+    }
+} 
+return 0;       
 }
