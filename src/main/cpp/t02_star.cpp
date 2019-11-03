0c81f99e86cddf956c6a034207ecf0bad28bc8fd
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-11-03 20:10:25 +0300

    Update t02_star.cpp

--- src/main/cpp/t02_star.cpp
+++ src/main/cpp/t02_star.cpp
@@ -24,6 +24,18 @@
 using namespace std;
 
 int t02_star() {
-
-    return 0;
-}
+    int N;
+     cin >> N;
+     int a[N][N];
+     for (int i = 1;i < N+1;i++){
+        for (int j = 1;j < N+1;j++){
+            if(j == ((N + 1) / 2)) cout << "*" << " ";
+            if (i == (N + 1) / 2) cout << "*" << " ";
+            else if ((i + j) == (N + 1)) cout << "*" << " ";
+            else if (i == j) cout << "*" << " ";
+            else cout << "." << " ";
+        }
+         cout << endl;
+     }
+     return 0;
+ }
