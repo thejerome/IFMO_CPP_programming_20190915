c4bc8a20c861a92dd87e528fb8e382e532fb958f
Надя <nadya.karandasheva.2000@mail.ru>
2019-11-03 16:11:22 +0300

    task-6 Карандашева Надежда

--- src/main/cpp/t06_cinema.cpp
+++ src/main/cpp/t06_cinema.cpp
@@ -39,9 +39,9 @@ cin>> k;
 number=n;
 bool flag=true;
 for (int i=0; i<=(n-1);i++){
-    int max=0;
     if (flag){
         for (int j=0; j<=(m-1); j++){
+        int max=0;
             if (a[i][j]==0) {
                 max++;
                 if (max >=k){
