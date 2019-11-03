c2bfeacd8c7925b7156ba27fbb27a7ff7326d4f7
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-11-03 16:33:42 +0300

    Update t07_snake.cpp

--- src/main/cpp/t07_snake.cpp
+++ src/main/cpp/t07_snake.cpp
@@ -39,13 +39,10 @@ int t07_snake() {
 
     for(int L = 0; L < P; L++){
         for(int H = 0; H < D; H++){
-         if (C[L][H]<10)
-        {cout << " ";}
-        if (C[L][H]<100)
-        {cout << "  ";}
-        if (C[L][H]<1000)
-        {cout << "  ";}
-        cout << C[L][H];
+         if (C[L][H]<10){cout << " ";}
+        if (C[L][H]<100) {cout << "  ";}
+        if (C[L][H]<1000){cout << "  ";}
+         cout << C[L][H];
         }
         cout << endl;
     }
