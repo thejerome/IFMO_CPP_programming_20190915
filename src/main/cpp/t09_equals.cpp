df270e509d7193f7b03915dfe944401ac7764758
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-03 17:38:55 +0300

    Update t09_equals.cpp

--- src/main/cpp/t09_equals.cpp
+++ src/main/cpp/t09_equals.cpp
@@ -26,5 +26,34 @@
 using namespace std;
 
 int t09_equals() {
-
+	int n, x, sum = 0;
+	cin >> n;
+	vector <int> a;
+	vector <int> counts;
+	vector <int> val;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+	}
+	val.push_back(a[0]);
+	counts.push_back(1);
+	for (int i = 1; i < n; i++) {
+		bool added = false;
+		for (int j = 0; j < val.size(); j++) {
+			if (val[j] == a[i]) {
+				counts[j]++;
+				added = true;
+			}
+		}
+		if (not added) {
+			val.push_back(a[i]);
+			counts.push_back(1);
+		}
+	}
+	for (auto now : counts) {
+		if (now > 1) {
+			sum = sum + (now * now - 3 * now) / 2 + now;
+		}
+	}
+	cout << sum;
+	return 0;
 }
