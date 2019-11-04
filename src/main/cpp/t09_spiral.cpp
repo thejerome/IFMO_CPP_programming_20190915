2275464a9a149859cca938ca4e79372fd8537f4d
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-04 16:05:52 +0300

    Update t09_spiral.cpp

--- src/main/cpp/t09_spiral.cpp
+++ src/main/cpp/t09_spiral.cpp
@@ -22,6 +22,41 @@
 using namespace std;
 
 int t09_spiral() {
-    
-    return 0;
+int n,m;
+cin>>n>>m;
+int a[n+2][m+2];
+for (int i=0;i<=n+1;i++){
+for (int k=0;k<=m+1;k++){
+a[i][k]=-1;}}
+for (int i=1;i<=n;i++){
+for (int k=1;k<=m;k++){
+a[i][k]=0;}}
+int B=0;
+int W=1;
+int S=0;
+while (B<n*m){
+while (a[W][S+1]==0){
+S++;
+B++;
+a[W][S]=B;}
+while (a[W+1][S]==0){
+W++;
+B++;
+a[W][S]=B;}
+while (a[W][S-1]==0){
+S--;
+B++;
+a[W][S]=B;}
+while (a[W-1][S]==0){
+W--;
+B++;
+a[W][S]=B;}}
+for (int i=1;i<=n;i++){
+for (int k=1;k<=m;k++){
+if (a[i][k]%10==a[i][k]) cout<<"   ";
+else if (a[i][k]%100==a[i][k]) cout<<"  ";
+else if (a[i][k]%1000==a[i][k]) cout<<" ";
+cout<<a[i][k];}
+cout << endl;}
+return 0;
 }
