1a5b7834f268db1f3bd350f3950fdcaeb3239868
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-04 04:36:59 +0300

    Update t03_bigger.cpp

--- src/main/cpp/t03_bigger.cpp
+++ src/main/cpp/t03_bigger.cpp
@@ -19,16 +19,15 @@
 using namespace std;
 
 int t03_bigger() {
-	int N;
-	cin >> N;
-	int A[N];
-	for (int i = 0; i < N; i++) {
-		cin>>A[i];
-		for(int i=1;i<N;i++){
-    if(A[i]>A[i-1])
-        cout<<A[i]<<" ";
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		cin>>a[i];}
+		for(int i=1;i<n;i++){
+    if(a[i]>a[i-1])
+        cout<<a[i]<<" ";
 }
 return 0;
-}
 }
 
