b29cdeb82a0fd3c9ac87c67daf6cf75f38cf36b2
Marygith <50752864+Marygith@users.noreply.github.com>
2019-11-04 20:10:41 +0300

    fixed spiral

--- src/main/cpp/t09_spiral.cpp
+++ src/main/cpp/t09_spiral.cpp
@@ -22,6 +22,27 @@
 using namespace std;
 
 int t09_spiral() {
+  int n = 0, m = 0, s = 0, h = 0, j = -1, ind = 0;
+  cin >> n >> m;
+  int a[n][m];
+  for (int i = 0; i < n; i++) {for (int k = 0; k < m; k++) {a[i][k] = 0;}}
+
+  while (s < n*m) {
+    if (ind == 0) {j++;  while (j < m and (a[h][j]<1 or a[h][j]>n*m)) {s++; a[h][j] = s; j++;} ind = 1;j--;}
+        if (s > n*m) {break;}
+    if (ind == 1) {h++; while (h < n and (a[h][j]<1 or a[h][j]>n*m)) { s++; a[h][j] = s;h++; } ind = 2;h--;}    
+        if (s > n*m) {break;}
+    if (ind == 2) {j--; while (j >= 0 and (a[h][j]<1 or a[h][j]>n*m)) {s++; a[h][j] = s;j--;} ind = 3;j++;}    
+        if (s > n*m) {break;}
+    if (ind == 3) {h--; while (h >= 0 and (a[h][j]<1 or a[h][j]>n*m)) {s++; a[h][j] = s;h--;} ind = 0;h++;}      
     
+  }
+  for (int i = 0; i < n; i++)
+   {
+  for (int k = 0; k < m; k++){
+  cout << a[i][k]; if(a[i][k] < 10) {cout << "   ";} else {cout << "  ";}
+  }
+  cout << endl;
+   }    
     return 0;
 }
