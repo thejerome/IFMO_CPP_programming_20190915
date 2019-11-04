482a4d622e1472aab3b3840e00952e5d987592d7
Marygith <50752864+Marygith@users.noreply.github.com>
2019-11-04 11:27:01 +0300

    fixed different

--- src/main/cpp/t06_different.cpp
+++ src/main/cpp/t06_different.cpp
@@ -19,5 +19,13 @@
 using namespace std;
 
 int t06_different() {
-
+  int n, s=0;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+  {cin >> a[i];}
+  for (int i = 0; i < n-1; i++)
+  {if (a[i+1] != a[i]) {
+    s ++;}}
+  cout << s; 
 }
