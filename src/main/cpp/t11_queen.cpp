11af4b37b73caf16f7a13700efad92a781f45e01
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-11-04 04:57:47 +0300

    Update t11_queen.cpp

--- src/main/cpp/t11_queen.cpp
+++ src/main/cpp/t11_queen.cpp
@@ -39,5 +39,16 @@
 using namespace std;
 
 int t11_queen() {
-
+int C[8][2];
+ for(int L = 0 ; L < 8;L++)
+for(int H = 0 ; H < 2;H++){
+cin >> C[L][H];}
+bool V = true;
+for(int L = 0; L < 8;L+=2){
+for(int H = 1; H < 8;H+=2){
+if (((C[L][0]== C[H][0]) || (C[L][1] == C[H][1])) || (abs(C[L][0]-C[H][0])) == (abs(C[L][1]- C[H][1])))
+V=false;}}
+ 
+if (V==true) {cout << "NO";}
+else {cout << "YES";}
 }
