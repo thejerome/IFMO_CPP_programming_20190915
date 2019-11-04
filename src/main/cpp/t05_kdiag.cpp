61cd48c2ad86d5f7a727c02ab760e1a3a02a02c7
Sasha Mikhailova <55459169+MikhSaas@users.noreply.github.com>
2019-11-04 19:59:52 +0300

    Update t05_kdiag.cpp

--- src/main/cpp/t05_kdiag.cpp
+++ src/main/cpp/t05_kdiag.cpp
@@ -38,6 +38,17 @@
 using namespace std;
 
 int t05_kdiag() {
-    
+    int b,c,d;
+    int a[100][100];
+    cin>>b>>c;
+    for (int i=0;i<b;i++) {
+        for (int j=0;j<c;j++) {
+            cin>>a[i][j];
+        }
+    }
+    cin>>d;
+    for (int i=0;i<b-1;i++) {
+        cout<<a[i+d][i]<<' ';
+    }
     return 0;
 }
