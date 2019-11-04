822dd421981fb3ad8f2129c3ae1cba4a5973f1b7
maandronova <55444394+maandronova@users.noreply.github.com>
2019-11-04 15:25:16 +0300

    Update t08_cycleswap.cpp

--- src/main/cpp/t08_cycleswap.cpp
+++ src/main/cpp/t08_cycleswap.cpp
@@ -25,12 +25,12 @@ int t08_cycleswap() {
     for (int i=0; i<n; i++) {
         cin >> arr[i]; }
 
-    last = arr[i-1];
-    for (i=n-1; i>0; i-=1) {
+    int last = arr[i-1];
+    for (int i=n-1; i>0; i-=1) {
         if (i == 0) arr[i] = last;
         else arr[i] = arr[i - 1];
     }
-    for (i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         cout << arr[i] << " ";
     }
     return 0;
