1b2e1a44f6c4a5a1a7ad8902cd2dc6dcf059ef96
maandronova <55444394+maandronova@users.noreply.github.com>
2019-11-04 15:22:41 +0300

    Update t04_neighbors.cpp

--- src/main/cpp/t04_neighbors.cpp
+++ src/main/cpp/t04_neighbors.cpp
@@ -27,8 +27,8 @@ int t04_neighbors() {
     for (int i=0; i<n; i++) {
         cin >> arr[i]; }
 
-    for (i=0; i<n; i++) {
-        if (((arr[i]<0) && (arr[i-1]>0)) || ((arr[i]>0)||(arr[i-1]<0))
+    for (int i=0; i<n; i++) {
+        if (((arr[i]<0) && (arr[i-1]>0)) || ((arr[i]>0)||(arr[i-1]<0)))
         cout << arr[i] << " " << arr[i-1];
     }
     return 0;
