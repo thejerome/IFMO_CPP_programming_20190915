1da13089d7631f3a8742cc280becc199b2b053d1
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-04 15:47:11 +0300

    Update t08_chess.cpp

--- src/main/cpp/t08_chess.cpp
+++ src/main/cpp/t08_chess.cpp
@@ -24,6 +24,26 @@
 using namespace std;
 
 int t08_chess() {
-    
-    return 0;
+int n, m, b;
+cin >> n >> m;
+int a[n][m];
+int qwer=1;
+for (int i=0;i<n;i++){
+for (int k=0;k<m;k++){
+a[i][k] = 0;}}
+for (int i=0;i<n;i++){
+if (i%2==1) b=1;
+else b=0;
+for (int k=b;k<m;k++){
+a[i][k]=qwer;
+qwer++;
+k++;}}
+for (int i=0;i<n;i++){
+for (int k=0;k<m;k++){
+if (a[i][k] % 10 == a[i][k]) cout <<"   ";
+else if (a[i][k]%100==a[i][k]) cout<<"  ";
+else if (a[i][k]%1000==a[i][k]) cout<<" ";
+cout<<a[i][k];}
+cout<<endl;}
+return 0;
 }
