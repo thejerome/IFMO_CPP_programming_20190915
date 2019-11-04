c5b46cce1b81eda9fa9b85274933305573c2d6c8
maandronova <55444394+maandronova@users.noreply.github.com>
2019-11-04 20:46:14 +0300

    Update t03_bigger.cpp

--- src/main/cpp/t03_bigger.cpp
+++ src/main/cpp/t03_bigger.cpp
@@ -27,7 +27,7 @@ int t03_bigger() {
 
         for (int i=0; i<n; i++) {
             if (arr[i] > arr[i-1])
-                cout << arr[i] << " ";
+                cout << arr[i] << ' ';
         }
         return 0;
     }
