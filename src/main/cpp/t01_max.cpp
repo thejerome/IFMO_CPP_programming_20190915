e7a5d8857f7a2006e74c5af69d02dc5184855837
Vasiliy Khan <nastiya@MacBook-Air-Anastasia.local>
2019-11-04 15:59:04 +0300

    sasasa

--- src/main/cpp/t01_max.cpp
+++ src/main/cpp/t01_max.cpp
@@ -27,18 +27,26 @@
 using namespace std;
 
 int t01_max() {
-    int a[10][10], n, m, s = 0, h = 0, l = 0;
+  int n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; i++)
-    for(int j = 0; j < m; j++)
-    { 
-        cin >> a[i][j];
-        if (a[i][j] > s)
-        {
-            s=a[i][j];
-            h=i;
-            l=j;
+    int a[100][100];
+
+    // чтение
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
         }
+    }
+    int max = a[0][0], max_i = 0, max_j = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] > max) {
+                max = a[i][j];
+                max_i = i;
+                max_j = j;
+            }
         }
+    }
+    cout << max_i << " " << max_j;
     return 0;
 }
--- src/main/cpp/t04_swap.cpp
+++ src/main/cpp/t04_swap.cpp
@@ -24,21 +24,21 @@
 
 using namespace std;
 
-int t04_swap() {
-int a[200][200], b,c,d,e;
-cin >> b >> c;
-for (int i=0; i < b; i++ )
-for (int j=0; j < c; j++)cin >> a [i][j];
-cin >> d,e;
-for (int j=0; j < c; j++)
+int a[101][101], m, n, l, k;
+cin >> n >> m;
+for(int i = 0; i < n; i++)
+for(int j = 0; j < m; j++)
+cin >> a[i][j];
+cin >> k >> l;
+for (int j = 0; j < n; j++)
 {
-    int x = a[j][d];
-    a[j][d] = a[j][e];
-    a[j][e] = x;
+    int x = a[j][l];
+    a[j][l] = a[j][k];
+    a[j][k] = x;
 }
-for(int i = 0; i < b; i++)
+for(int i = 0; i < n; i++)
 {
-for(int j = 0; j < c; j++)
+for(int j = 0; j < m; j++)
 cout << a[i][j] << " ";
 cout << endl;
 }
