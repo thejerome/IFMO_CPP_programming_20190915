a493cfca1f24627ed8057333156ebc5427f4b012
abdalla20736 <55684848+abdalla20736@users.noreply.github.com>
2019-11-04 05:06:27 +0300

    Update t05_minodd.cpp

--- src/main/cpp/t05_minodd.cpp
+++ src/main/cpp/t05_minodd.cpp
@@ -24,7 +24,6 @@
 #include <iostream>
 #include <cmath>
 
-
 using namespace std;
 
 int t05_minodd() {
@@ -34,18 +33,13 @@ int C[P];
 for(int L = 0;L<P;L++)
 {
 
-
-     cin >> C[L];
-
-
-
+    cin >> C[L];
     if((C[L]%2!=0) && (Z==0 || Z > C[L]))
     {
 
        Z=C[L];
 
     }
-
 
  }
  cout << Z;
