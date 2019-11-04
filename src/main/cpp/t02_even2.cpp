601d91c4feafbc875e324a0c1b841ed6fa8fb05a
maandronova <55444394+maandronova@users.noreply.github.com>
2019-11-04 15:18:22 +0300

    Update t02_even2.cpp

--- src/main/cpp/t02_even2.cpp
+++ src/main/cpp/t02_even2.cpp
@@ -25,7 +25,7 @@ int t02_even2() {
     for (int i=0; i<n; i++) {
         cin >> arr[i];
     }
-    for (i=0; i<n; i++) {
-        if (a[i] % 2 = 0) cout << arr[i]; }
+    for (int i=0; i<n; i++) {
+        if (arr[i] % 2 = 0) cout << arr[i]; }
     return 0;
     }
