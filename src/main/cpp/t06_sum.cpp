89d093a4c62572def416e867b8c16c84d5681dc0
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-11-06 16:43:16 +0300

    Update t06_sum.cpp

--- src/main/cpp/t06_sum.cpp
+++ src/main/cpp/t06_sum.cpp
@@ -21,8 +21,16 @@
 #include <cmath>
 
 using namespace std;
-
+int sum() {
+   int n;
+   cin >> n;
+   if (n == 0) {
+      return 0;
+       }
+   return n + sum();
+}
 int t06_sum() {
-
+int main() {
+    cout << sum();
     return 0;
 }
