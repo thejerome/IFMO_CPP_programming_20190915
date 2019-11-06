eb94e07d4640237e0cd42cfb7e81cb35e7afe7de
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-11-06 16:40:57 +0300

    Update t05_power.cpp

--- src/main/cpp/t05_power.cpp
+++ src/main/cpp/t05_power.cpp
@@ -24,12 +24,20 @@
 
 #include <iostream>
 #include <cmath>
-
+#include <iomanip>
 using namespace std;
 
 //function power
-
+double power(double a, int n) {
+    if (n == 0) return 1;
+    if (n % 2 == 0) return power(a*a, n/2);
+    return a * power(a, n - 1);
+}
 int t05_power() {
-
+    double  a;
+    int n;
+    cin >> a >> n;
+    cout << fixed << setprecision(5);
+    cout << power(a,n);
     return 0;
 }
