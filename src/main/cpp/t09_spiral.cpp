f959b66ad8310ed859ec6a22efd31b577f59898c
Polina <55378557+chipa371@users.noreply.github.com>
2019-11-07 10:44:05 +0300

    Update t09_spiral.cpp

--- src/main/cpp/t09_spiral.cpp
+++ src/main/cpp/t09_spiral.cpp
@@ -61,7 +61,7 @@ int t09_spiral() {
     }
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            cout << a[i][j] << setw(5);
+            cout << setw(5) << a[i][j];
         }
         cout << "\n";
     }
