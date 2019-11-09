1ca33269bfba1cb38de31db042c5d29bfe50e9c3
afruza <56043168+afruza@users.noreply.github.com>
2019-11-09 23:08:08 +0500

    Update t08_cycleswap.cpp

--- src/main/cpp/t08_cycleswap.cpp
+++ src/main/cpp/t08_cycleswap.cpp
@@ -19,15 +19,16 @@
 using namespace std;
 
 int t08_cycleswap() {
-  int i,n=0;
-  cin >> n;
-  int a[n];
-  for ( i=0;i<n;i++) {
-    cin >> a[i];
-  }
- cin >> a[0];
-  for (i=0;i<n;i++) { 
-    cout << a[i] << " ";
-  }
-  return 0;
+     int n, temp;
+     cin >> n;
+     int a[n];
+     for (int i = 0; i < n; i++) {
+         cin >> a[i];}
+     temp = a[n-1];
+     for (int i = n-1; i > 0; i--){
+         a[i] = a[i - 1];}
+     a[0] = temp;
+     for (int i = 0; i < n; i++) {
+         cout << a[i] << ' ';}
+    return 0;
 }
