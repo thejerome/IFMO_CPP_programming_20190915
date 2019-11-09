3b1983b67713f11287785c5b9edd782204b33a09
Надя <nadya.karandasheva.2000@mail.ru>
2019-11-09 18:42:12 +0300

    task-7 Карандашева Надежда

--- src/main/cpp/t01_min.cpp
+++ src/main/cpp/t01_min.cpp
@@ -21,11 +21,20 @@
 
 using namespace std;
 
-//function min 
-
-//function min4 
-
+//function min
+int min(int a,int b) {
+    if (a>b) return b;
+    else return a;
+}
+//function min4
+int min4(int a,int b, int c,int d){
+    a=min(a,b);
+    c=min(c,d);
+    return min(a,c);
+}
 int t01_min() {
-
+int a,b,c,d;
+cin >>a>>b>>c>>d;
+cout << min4(a,b,c,d);
     return 0;
 }
