7c573a16b99c0033759bac50ae0037e5b595b5c6
Надя <nadya.karandasheva.2000@mail.ru>
2019-11-09 19:43:23 +0300

    task-7 Карандашева Надежда

--- src/main/cpp/t04_area.cpp
+++ src/main/cpp/t04_area.cpp
@@ -34,13 +34,17 @@ using namespace std;
 
 //function IsPointInArea
 bool IsPointInCircle(float x,float y){
-    if (4 - sqrt(pow(x+1,2)+pow(y-1,2))>=0) return true;
+    if (2 - sqrt(pow(x+1,2)+pow(y-1,2))>=0) return true;
+    else return false;
+}
+bool IsPointOnCircle(float x,float y){
+    if (sqrt(pow(x+1,2)+pow(y-1,2))==2) return true;
     else return false;
 }
 bool IsPointInArea(float x,float y){
     bool flag=false;
-    if ((y>=0)and(y>=-x)and(y>=(2*x-1)and(IsPointInCircle(x,y)))) flag=true;
-    if ((y<=0)and(y<=-x)and(y<=(2*x-1)and(not IsPointInCircle(x,y)))) flag=true;
+    if ((y>=0)and(y>=-x)and(y>=(2*x+2))and(IsPointInCircle(x,y))) flag=true;
+    if ((y<=0)and(y<=-x)and(y<=(2*x+2))and(IsPointOnCircle(x,y))) flag=true;
     return flag;
 }
 
