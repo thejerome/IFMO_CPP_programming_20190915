1873319066e0d8d762e53263c0c9ce71ca5f52d0
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-11-10 16:33:44 +0300

    Update t03_toggle.cpp

--- src/main/cpp/t03_toggle.cpp
+++ src/main/cpp/t03_toggle.cpp
@@ -24,5 +24,15 @@
 using namespace std;
 
 int t03_toggle() {
-
+  char c;
+    cin >> c;
+    if ( c >= 'a' && c <= 'z') {
+      c = c - 'a' + 'A';
+    }
+     
+    else  if ( c >= 'A' && c <= 'Z') {
+      c = c - 'A' + 'a';
+    }
+    cout << c;
+  return 0;
 }
