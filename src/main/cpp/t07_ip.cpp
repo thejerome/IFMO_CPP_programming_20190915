46bb12a1cbad9f4b5ec71f98415fbb3414516c9f
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-11-10 16:37:38 +0300

    Update t07_ip.cpp

--- src/main/cpp/t07_ip.cpp
+++ src/main/cpp/t07_ip.cpp
@@ -29,10 +29,75 @@
 
 #include "t07_ip.h"
 #include <iostream>
-
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int t07_ip() {
-
+  string s, x1, x2, x3, x4;
+    bool b = 1;
+    char c;
+    getline(cin, s);
+    int i, j, len, pointcount = 0, length, y1, y2, y3, y4, length1, length2, length3, length4;
+    len = s.size();
+     vector <int> pointid(3);
+     for (i = 0; i < len; i++) {
+         if (s[i] == '.') {
+             pointid[pointcount] = i;
+             pointcount++;
+         }
+     }
+    
+    if (pointcount != 3) {
+        cout << "NO";
+    }
+    
+    else {
+   x1 = s.substr(0, pointid[0]);                          length1 = x1.size();       
+   x2 = s.substr(pointid[0]+1, pointid[1]-pointid[0]-1);  length2 = x2.size();       
+   x3 = s.substr( pointid[1]+1, pointid[2]-pointid[1]-1); length3 = x3.size();    
+   x4 = s.substr( pointid[2]+1, len-pointid[2]-1);        length4 = x4.size();      
+ //-------------------------       
+        for (auto c: x1) {
+         if (c < '0' || c > '9') {
+             b = 0;
+         }
+        }
+        for (auto c: x2) {
+         if (c < '0' || c > '9') {
+             b = 0;
+         }
+        }
+        for (auto c: x3) {
+         if (c < '0' || c > '9') {
+             b = 0;
+         }
+        }
+        for (auto c: x4) {
+         if (c < '0' || c > '9') {
+             b = 0;
+         }
+        }
+    //-----------------------      
+      if (!b || length1 * length2 * length3 * length4 == 0) {
+       cout << "NO";   
+      }
+      else {
+        y1 = stoi(x1);   
+        y2 = stoi(x2);
+        y3 = stoi(x3);
+        y4 = stoi(x4); 
+        if ( y1<0 || y1>255 || y2<0 || y2>255 || y3<0 || y3>255 || y4<0 || y4>255) {
+         b = 0;   
+        }
+        if (!b) {
+         cout << "NO";  
+        }
+          else {
+           cout << "YES";   
+          }
+      }
+   }
+  return 0;
 }
