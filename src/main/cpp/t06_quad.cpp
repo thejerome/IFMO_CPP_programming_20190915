2757d54e39b7eb9d30232f3d068009fbfdf452c3
Marygith <50752864+Marygith@users.noreply.github.com>
2019-11-10 11:29:46 +0300

    fixed quad

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -21,11 +21,11 @@
 using namespace std;
 
 int t06_quad() {
-    float a, b, c, d, x1, x2;
+    float a, b, c, d, x1;
     cin >> a >> b >> c;
     d = b*b - 4*a*c;
     if (d >= 0) {
         x1 = (-b + sqrt(d))/2/a;
-        x2 = (-b - sqrt(d))/2/a;
-        if(x1 == x2) {cout << x1;} else {if(x1 > x2) {cout << x2 << " " << x1;} else {cout << x1 << " " << x2;}}}
+        c = (-b - sqrt(d))/2/a;
+        if(x1 == c) {cout << x1;} else {if(x1 > c) {cout << c << " " << x1;} else {cout << x1 << " " << c;}}}
 };
