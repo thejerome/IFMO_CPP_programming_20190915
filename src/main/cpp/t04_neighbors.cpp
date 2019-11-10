b11d58a9a6ed491a2d3d2cfa4b08ae722eb236ee
chromyy <55399522+chromyy@users.noreply.github.com>
2019-11-11 02:25:20 +0300

    Update t04_neighbors.cpp

--- src/main/cpp/t04_neighbors.cpp
+++ src/main/cpp/t04_neighbors.cpp
@@ -24,14 +24,12 @@ int t04_neighbors() {
     int n;
     cin >> n;
     int arr[n];
-    for (int i = 1; i < n; i++){
+    for (int i = 0; i < n; i++){
         cin >> arr[i];
-        if (arr[i] * arr[i-1] > 0){
+        if (arr[i] * arr[i-1] > 0 && (i > 0)){
             cout << arr[i-1] << " " << arr[i];
             break;
         };
     };
-        return 0;
-
 
 }
