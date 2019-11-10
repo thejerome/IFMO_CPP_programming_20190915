e63fa6102765bde0d8ce40eb7faaaf99c23b1036
chromyy <55399522+chromyy@users.noreply.github.com>
2019-11-11 02:17:12 +0300

    Update t08_cycleswap.cpp

--- src/main/cpp/t08_cycleswap.cpp
+++ src/main/cpp/t08_cycleswap.cpp
@@ -19,5 +19,20 @@
 using namespace std;
 
 int t08_cycleswap() {
+    int n, tmp;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    };
+    tmp = arr[n-1];
+    for (int i = n-1; i > 0; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = tmp;
+
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    };
 
 }
