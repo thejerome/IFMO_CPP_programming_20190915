1eedbc65befdd5f0d228d88ef217e9008ea0eac0
Yurikyakyt <55650447+Yurikyakyt@users.noreply.github.com>
2019-11-11 21:36:25 +0300

    Update t05_power.cpp

--- src/main/cpp/t05_power.cpp
+++ src/main/cpp/t05_power.cpp
@@ -28,8 +28,18 @@
 using namespace std;
 
 //function power
-
-int t05_power() {
-
-    return 0;
+double power(double a,int n){
+    if (n == 0){
+        return 1;
+    }else if (n>0){
+        return (a*power(a,(n-1)));
+    }else if (n<0){
+        return 1/(a*power(a,(n-1)));    
+    }    
+}
+int main(){
+    double a;
+    int n;
+    cin >> a >> n;
+    cout << power(a,n);   
 }
