a93ed37c0baa832b8c19ccff4df5647fd535429f
Maxim Dexter <d3gnil@gmail.com>
2019-11-12 22:25:48 +0300

    completed

--- src/main/cpp/t06_longest.cpp
+++ src/main/cpp/t06_longest.cpp
@@ -9,7 +9,7 @@
 //Выведите самое длинное слово. Если таких слов несколько, то выводить нужно, которое встречается раньше.
 //
 //Sample Input:
-//
+//        8  11 14
 //Everyone of us has all we need
 //Sample Output:
 //
@@ -17,10 +17,31 @@
 
 #include "t06_longest.h"
 #include <iostream>
+#include <string>
 
 
 using namespace std;
 
 int t06_longest() {
+    string s;
+    getline(cin, s);
+    int end, max, beg, maxbeg, n, maxend;
+    n = s.length();
+    maxend = s.find (' ');
+    max = maxend;
+    beg = maxend + 1;
+    maxbeg = 0;
 
+    for (end = s.find (' ', beg); end != -1; end = s.find (' ', beg) ) {
+        if (end - beg > max) {
+            max = end - beg; maxbeg = beg; maxend = end;
+        }
+        beg = end + 1;
+    }
+    end = n;
+    if (end-beg > max) {
+        max = end - beg; maxbeg = beg;
+    }
+    cout << s.substr (maxbeg, max);
+    return 0;
 }
