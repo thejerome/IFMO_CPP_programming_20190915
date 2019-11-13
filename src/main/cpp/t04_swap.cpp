305f7ec5c71764ac07f4540de216d6ca720150fd
afruza <56043168+afruza@users.noreply.github.com>
2019-11-13 05:50:34 +0500

    Update t04_swap.cpp

--- src/main/cpp/t04_swap.cpp
+++ src/main/cpp/t04_swap.cpp
@@ -20,11 +20,34 @@
 
 #include "t04_swap.h"
 #include <iostream>
-
-
 using namespace std;
-
 int t04_swap() {
-    
+    int n, m;
+    cin>>n>>m;
+    int a[n][m];
+    for(int i = 0; i<n;i++)
+    {
+        for(int j = 0; j<m;j++)
+        {
+            cin>>a[i][j];
+            }
+        }
+    int x, y;
+    cin>>x>>y;
+    for(int i = 0;i<n;i++)
+    {
+        int k = a[i][x];
+        a[i][x] = a[i][y];
+        a[i][y] = k;
+    }
+    for(int i =0; i<n;i++)
+    {
+        
+        for(int j = 0; j<m;j++)
+        {
+            cout<<a[i][j]<<' ';
+            }
+            cout<<endl;
+        }
     return 0;
 }
