15306efe7658d6b10816d5c65e4f6a92c20199f9
afruza <56043168+afruza@users.noreply.github.com>
2019-11-13 16:42:05 +0500

    Update t07_quad2.cpp

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -27,14 +27,14 @@ int t07_quad2() {
         d = pow(b,2) - 4 * a * c;
         if ( d > 0 ) {
             k = ( b * (-1) - sqrt(d) ) / ( 2 * a );
-            k = ( b * (-1) + sqrt(d) ) / ( 2 * a );
+            z = ( b * (-1) + sqrt(d) ) / ( 2 * a );
             if ( k > z ) cout << 2 << " " << z << " " << k;
             else cout << 2 << " " << k << " " << z;
         }
         else if ( d == 0 ) cout << 1 << " " << b * (-1) / ( 2 * a );
         else cout << 0;
     }
-    else if ( b != 0 ) cout << 1 << " " << c * (-1) / b;
+    else if ( a==0 && b != 0 ) cout << 1 << " " << c * (-1) / b;
     else if ( a == 0 && c == 0 && b != 0 ) cout << 1 << " " << 0;
     else if ( a == 0 &&  b == 0 && c != 0 ) cout << 0; 
     else if (a == 0 && b == 0 && c == 0) cout << 3;
