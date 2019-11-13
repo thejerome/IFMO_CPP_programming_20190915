50336312302d3314e32ef4283f7f1424f8430e8e
deadinsidezxc <lordsymon99@mail.ru>
2019-11-13 19:07:04 +0300

    OmegyLUL just testing

--- src/main/cpp/t05_minodd.cpp
+++ src/main/cpp/t05_minodd.cpp
@@ -38,14 +38,14 @@ int t05_minodd() {
         if (a[i] % 2 == 1) {
             f = true;
             min = a[i];
-            cout << "min nashel: " << min;
+            cout << min;
         };
     };
 
     for (int i =0; i < n; i++) {
         if ((a[i] % 2 == 1) && (min > a[i])) {
             min = a[i];
-            cout << "ponenyal min na: " << min;
+            cout << min;
         };
     };
 
