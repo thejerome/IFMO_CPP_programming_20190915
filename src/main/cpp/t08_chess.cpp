380cdcd507c63de5849a76afebea49291e69dec2
afruza <56043168+afruza@users.noreply.github.com>
2019-11-13 06:35:22 +0500

    Update t08_chess.cpp

--- src/main/cpp/t08_chess.cpp
+++ src/main/cpp/t08_chess.cpp
@@ -24,6 +24,33 @@
 using namespace std;
 
 int t08_chess() {
-    
+    int n, m, k;
+    cin >> n >> m;
+    int a[n][m];
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            a[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 1) k = 1;
+        else k = 0;
+        for (int j = k; j < m; j++) {
+            a[i][j] = num;
+            num++;
+            j++;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] % 10 == a[i][j]) cout << "   ";
+            else if (a[i][j] % 100 == a[i][j]) cout << "  ";
+            else if (a[i][j] % 1000 == a[i][j]) cout << " ";
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
