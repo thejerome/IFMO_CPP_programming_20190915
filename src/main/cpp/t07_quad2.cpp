e410d6fa8b26b4feb7bdd6e74e9443194266a11a
afruza <56043168+afruza@users.noreply.github.com>
2019-11-13 17:25:57 +0500

    Update t07_quad2.cpp

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -38,7 +38,7 @@ int t07_quad2() {
     }
     else if ( b != 0 ) cout << 1 << " " << c * (-1) / b;
     else if ( c == 0 && b != 0 ) cout << 1 << " " << 0;
-    else if ( b == 0 && c != 0 ) cout << 0; 
+    else if ( c != 0 ) cout << 0; 
     else if ( b == 0 && c == 0) cout << 3;
     return 0;
 };
