d627ecdb6409b63293ef7c59e2b808218f2c86a3
afruza <56043168+afruza@users.noreply.github.com>
2019-11-15 00:47:49 +0500

    Update t03_circle.cpp

--- src/main/cpp/t03_circle.cpp
+++ src/main/cpp/t03_circle.cpp
@@ -34,12 +34,19 @@
 
 #include <iostream>
 #include <cmath>
-
 using namespace std;
+bool IsPointInCircle(double x, double y, double xc, double yc, double r) {
+    return abs(x - xc)*abs(x - xc) + abs(y - yc)*abs(y - yc) <= r*r;
+}
 
-//function IsPointInCircle
-
-int t03_circle() {
+int t03_circle() { 
+    double  x, y, xc, yc, r;
+    cin >> x >> y >> xc >> yc >> r;
+    if (IsPointInCircle(x, y, xc, yc, r)) {
+        cout << "YES";
+    }
+    else
+        cout << "NO";
 
     return 0;
 }
