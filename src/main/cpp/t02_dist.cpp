5178d4ec157f2568902dfdd9f6979dc76024dca9
afruza <56043168+afruza@users.noreply.github.com>
2019-11-15 00:36:04 +0500

    Update t02_dist.cpp

--- src/main/cpp/t02_dist.cpp
+++ src/main/cpp/t02_dist.cpp
@@ -17,13 +17,18 @@
 //1.41421
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
-
 using namespace std;
-
-//function distance
-
-int t02_dist() {
-
+double distance(double x1, double y1, double x2, double y2) {
+    double a = abs(x1 - x2);
+    double b = abs(y1 - y2);
+    return sqrt(pow(a,2) + pow(b,2));
+}
+int t02_dist() { 
+    double  x1, y1,  x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << fixed << setprecision(5);
+    cout << distance(x1, y1, x2, y2);
     return 0;
 }
