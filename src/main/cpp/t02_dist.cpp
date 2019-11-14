bbb85bf49dd425d94ffd8d58e5e6bfdf3837bd85
Polina <55378557+chipa371@users.noreply.github.com>
2019-11-14 18:51:54 +0300

    Update t02_dist.cpp

--- src/main/cpp/t02_dist.cpp
+++ src/main/cpp/t02_dist.cpp
@@ -18,12 +18,25 @@
 
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 //function distance
+double function_distance(double a, double b, double c, double d){
+    double p;
+    double l;
+    double s;
+    l = abs(a-c);
+    s = abs(b-d);
+    p = sqrt(l*l + s*s);
+    return p;
+}
 
 int t02_dist() {
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << setprecision(6) << function_distance(x1, y1, x2, y2);
 
     return 0;
 }
