1b4feffa0573c1fd441b38a5de6574cfd7540089
afruza <56043168+afruza@users.noreply.github.com>
2019-11-15 05:00:17 +0500

    Update t08_queen.cpp

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -19,10 +19,36 @@
 
 #include <iostream>
 #include <vector>
-
+#include <cmath>
+#include <iomanip>
 using namespace std;
+int board[10];
 
+bool check(int a, int b, int c) {
+    if (c == a) return true;
+    else return board[c] != b && (a - c) != (b - board[c]) && (a - c) != (board[c] - b) && check(a, b, c + 1);
+}
 int t08_queen(){
+int put_queen(int n, int a, int b) {
+    if (a == n) return 1;
+    else {
+        if (b < n) {
+            int r = 0;
+            if (check(a, b, 0)) {
+                board[a] = b;
+                r = put_queen(n, a + 1, 0);
+            }
+            return r + put_queen(n, a, b + 1);
+        }
+        else return 0;
+    }
+}
 
+int main() {
+    int n;
+    cin >> n;
+    cout << put_queen(n, 0, 0);
+    return 0;
+}
     return 0;
 }
