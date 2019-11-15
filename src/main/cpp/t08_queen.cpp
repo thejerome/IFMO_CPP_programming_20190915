956c6bfbb71de98c8e55e478483def8e78fd0ac4
afruza <56043168+afruza@users.noreply.github.com>
2019-11-15 15:41:33 +0500

    Update t08_queen.cpp

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -19,10 +19,31 @@
 
 #include <iostream>
 #include <vector>
-
+#include <iomanip>
+#include <cmath>
 using namespace std;
-
+int g[10];
+bool arr(int a, int b, int k) {
+    if (k == a) return true;
+    else return arr(a, b, k + 1) && (a - k) != (b - g[k]) && (a - k) != (g[k] - b) && g[k] != b;
+}
+int ferz(int n, int a, int b) {
+    if (a == n) return 1;
+    else {
+        if (b < n) {
+            int j = 0;
+            if (arr(a, b, 0)) {
+                g[i] = b;
+                j = ferz(n, a + 1, 0);
+            }
+            return r + ferz(n, a, b + 1);
+        }
+        else return 0;
+    }
+}
 int t08_queen() {
-
+int n;
+    cin >> n;
+    cout << ferz(n, 0, 0);
     return 0;
 }
