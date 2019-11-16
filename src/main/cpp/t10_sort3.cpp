6703bf2b46d9f6a8444d3f7c988f8acd597c6dd1
homa207 <homa207@mail.ru>
2019-11-16 22:36:00 +0300

    modified:   src/main/cpp/t10_sort3.cpp

--- src/main/cpp/t10_sort3.cpp
+++ src/main/cpp/t10_sort3.cpp
@@ -22,7 +22,6 @@ int t10_sort3() {
 	int a, b, c;
 	cin >> a >> b >> c;
 	if (a >= b && b >= c) cout << c << " " << b << " " << a;
-	else if (a >= b && b >= c) cout << c << " " << b << " " << a;
 	else if (b >= c && c >= a) cout << a << " " << c << " " << b;
 	else if (b >= a && a >= c) cout << c << " " << a << " " << b;
 	else if (c >= b && b >= a) cout << a << " " << b << " " << c;
