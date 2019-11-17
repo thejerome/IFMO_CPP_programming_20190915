642a04163b917d3404eb48c6c29646e0f768ac2e
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-11-17 22:09:16 +0300

    Update t03_points.cpp

--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -23,10 +23,33 @@
 
 #include "t03_points.h"
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct point {
+ int x, y, distance;
+};
+
+bool cmp ( point a, point b) {
+  return a.distance < b.distance;  
+}
 int t03_points() {
-    
+ int n, i, temp1, temp2, dis;
+    cin >> n;
+    vector <point> a(n);
+    for (i = 0; i < n; i++) {
+        cin >> temp1 >> temp2;
+        dis = temp1*temp1 + temp2*temp2;        
+        point struct_temp;        
+        struct_temp.x = temp1;
+        struct_temp.y = temp2;
+        struct_temp.distance = dis;
+        a[i] = struct_temp;
+    }
+    stable_sort(a.begin(), a.end(), cmp);
+    for (auto i: a) {
+     cout << i.x << " " << i.y << endl; 
+    }    
 }
