eb2fc3e55da6171649f971b1aae8c5ff87c61af9
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-11-17 19:33:19 +0300

    Update t08

--- src/main/cpp/t08_cycleswap.cpp
+++ src/main/cpp/t08_cycleswap.cpp
@@ -19,5 +19,21 @@
 using namespace std;
 
 int t08_cycleswap() {
+  int n,i,k;
+ cin >> n;
+ int a[n];
+ for( i = 0; i < n ; i++) {
+     cin >> a[i];
+     }
+ k=a[n-1];
+ for( i = n-1; i > 0 ; i--) {
+     a[i] = a[i-1];
+ }
+ a[0]=k;
+for( i = 0; i < n ; i++) {
+     cout << a[i] << ' ';
+     }
+return 0;
+}
 
 }
