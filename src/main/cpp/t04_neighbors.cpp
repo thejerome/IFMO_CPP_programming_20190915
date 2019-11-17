e226a32ba98ba3e8adc29bdc58f86b4f4782eb3a
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-11-17 18:43:32 +0300

    Update t04

--- src/main/cpp/t04_neighbors.cpp
+++ src/main/cpp/t04_neighbors.cpp
@@ -21,5 +21,21 @@
 using namespace std;
 
 int t04_neighbors() {
+  int n,i;
+ cin >> n;
+ int a[n];
+ for( i = 0; i < n ; i++) {
+     cin >> a[i];
+     }
+ for( i = 1; i < n ; i++ ) {
+     if (a[i] > 0 and a[i-1] > 0) {
+         cout << a[i] << ' ' << a[i-1];
+         break;
+        }
+     if (a[i] < 0 and a[i-1] < 0) {
+         cout << a[i] << ' ' << a[i-1];
+         break;
+        }
+    }
 
 }
