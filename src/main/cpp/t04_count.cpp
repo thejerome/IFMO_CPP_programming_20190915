9b511b782a1060a4479ac7f9a7c6dd9a02a2539b
annapetrova947 <apetrova.01@mail.ru>
2019-11-17 20:14:47 +0500

    modified:   src/main/cpp/t04_count.cpp

--- src/main/cpp/t04_count.cpp
+++ src/main/cpp/t04_count.cpp
@@ -32,12 +32,12 @@ int t04_count() {
 	string s;
 	int cnt = 1, i = 0;
 	getline(cin, s);
-	while ((int)(s.find(" ", i)) > -1) {
-		cnt++;
-		i = s.find(" ", i) + 1;
+	for (int i=0; i < s.size();i++) {
+		if (s[i] == ' ') {
+			cnt++;
+
+		}
 	}
 	cout << cnt;
 	return 0;
-	
-
 }
