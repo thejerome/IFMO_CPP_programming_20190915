3db88bcb79dc349f1f9ef97f879ab40ccc85c388
Anatoly122 <tmail2020@mail.ru>
2019-11-18 20:00:06 +0300

    modified:   src/main/cpp/t08_queen.cpp

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -18,11 +18,53 @@
 //92
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int t08_queen(){
+int board[10][10] = { 0 };
+int res = 0, n = 0;
 
-    return 0;
+void SQueen(int i, int j)
+{
+	for (int x = 0; x < n; ++x)
+	{
+		++board[x][j];
+		++board[i][x];
+		int f = j - i + x;
+		if (f >= 0 && f < n) { ++board[x][f]; }
+		f = j + i - x;
+		if (f >= 0 && f < n) { ++board[x][f]; }
+	}
+}
+
+void DQueen(int i, int j)
+{
+	for (int x = 0; x < n; ++x)
+	{
+		--board[x][j];
+		--board[i][x];
+		int f = j - i + x;
+		if (f >= 0 && f < n) { --board[x][f]; }
+		f = j + i - x;
+		if (f >= 0 && f < n) { --board[x][f]; }
+	}
+}
+
+void Q(int t)
+{
+	for (int i = 0; i < n; i++)
+	{
+		bool set = false;
+		if (board[i][t] == 0 && t != n - 1) { SQueen(i, t); set = true; Q(t + 1); }
+		if (board[i][t] == 0 && t == n - 1) { res++; }
+		if (set) { DQueen(i, t); }
+	}
+
+}
+
+int t08_queen()
+{
+	cin >> n;
+	Q(0);
+	cout << res;
 }
