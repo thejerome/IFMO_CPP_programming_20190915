09404ced4056da68359ede51018565104cee5509
Yurikyakyt <55650447+Yurikyakyt@users.noreply.github.com>
2019-11-18 19:06:53 +0300

    Update t01_char.cpp

--- src/main/cpp/t01_char.cpp
+++ src/main/cpp/t01_char.cpp
@@ -30,5 +30,12 @@
 using namespace std;
 
 int t01_char() {
-
+    char a;
+    cin >> a;
+    cout << int(a);
+    if ((int(a)<58)&&(int(a)>47)){
+        cout << "yes";
+    }else{
+        cout << "no";
+    }
 }
