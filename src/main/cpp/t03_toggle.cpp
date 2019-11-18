71bf026cc1107116f10703899aa8e572b5bdbdd1
Leopold0_0 <55684777+lenix161@users.noreply.github.com>
2019-11-18 16:34:15 +0300

    hey codacy come here

--- src/main/cpp/t03_toggle.cpp
+++ src/main/cpp/t03_toggle.cpp
@@ -24,12 +24,13 @@
 using namespace std;
 
 int t03_toggle() {
- char n;
+    char n;
     cin >> n;
     if(n >= 97 && n <= 122)
         n = n - 32;
     else if(n >= 65 && n <= 90)
         n = n + 32;
     cout << n;
-    return 0;
+
+return 0;
 }
