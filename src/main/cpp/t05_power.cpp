9fc9505a465be29640c33beeceb5024d85acde8a
Zombe2203 <zombe2203@gmail.com>
2019-11-19 10:46:58 +0300

    t05 done

--- src/main/cpp/t05_power.cpp
+++ src/main/cpp/t05_power.cpp
@@ -28,8 +28,30 @@
 using namespace std;
 
 //function power
+double power(double a, int n);
+double power(double a, int n){
+    if (n==1){
+        return a;
+    }
+    if (n == 0){
+        return 1;
+    }
+    if (n == -1){
+        return 1/a;
+    }
+    if  (n > 1){
+        a=a*a;
+        return power(a, n-1);
+    }
+    if (n < 0){
+        a = 1/a;
+        return power (a, n+1);
+    }
+}
 
 int t05_power() {
-
+    double a=0; int n=0;
+    cin >>a >>n;
+    cout <<power(a,n);
     return 0;
 }
