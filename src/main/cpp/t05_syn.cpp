6e0bf08fbe767aa466ab5e1b77cfecbffe6fd6db
afruza <56043168+afruza@users.noreply.github.com>
2019-11-19 22:57:39 +0500

    Update t05_syn.cpp

--- src/main/cpp/t05_syn.cpp
+++ src/main/cpp/t05_syn.cpp
@@ -27,10 +27,22 @@
 
 #include "t05_syn.h"
 #include <iostream>
-
-
+#include <map>
+#include <string>
 using namespace std;
-
 int t05_syn() {
+map <string, string> m;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string x, y;
+        cin >> x >> y;
+        m[x] = y;
+        m[y] = x;
+    }
+    string s;
+    cin >> s;
+    cout << m[s];
+    return 0;
 
 }
