a6809faf2ffd4b7568e1af7a7706cd0cc8d3fac5
Zombe2203 <zombe2203@gmail.com>
2019-11-20 17:40:38 +0300

    t07 done

--- src/main/cpp/t07_fib.cpp
+++ src/main/cpp/t07_fib.cpp
@@ -18,8 +18,18 @@
 using namespace std;
 
 // function fib
-
-int t07_fib()
-{
+int fib (int n);
+int fib (int n){
+    if ((n==1) or (n==2)){
+        return 1;
+    } else {
+        return (fib(n-1) + fib(n-2));
+    }
+}
+int t07_fib(){
+    int n;
+    cin >>n;
+    n = fib(n);
+    cout <<n;
     return 0;
 }
