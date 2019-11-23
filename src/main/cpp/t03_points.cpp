6e82857d0ee13dd71c682972d67458a5f6bb26ce
Maxim Dexter <d3gnil@gmail.com>
2019-11-24 01:07:42 +0300

    completed

--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -23,10 +23,35 @@
 
 #include "t03_points.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
 
 using namespace std;
+struct dot {
+    int x, y;
+    double distance;
+};
+
+bool comparison (dot a, dot b) {
+    return (a.distance < b.distance);
+}
 
 int t03_points() {
-    
+    int n;
+    cin >> n;
+    vector <dot> a(n);
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        dot new_d;
+        cin >> x >> y;
+        new_d.x = x;
+        new_d.y = y;
+        new_d.distance = sqrt(pow(x,2) + pow(y, 2));
+        a[i] = new_d;
+    }
+    sort (a.begin(), a.end(), comparison);
+    for (auto elem: a) cout << elem.x << " " << elem.y << endl;
+    return 0;
 }
