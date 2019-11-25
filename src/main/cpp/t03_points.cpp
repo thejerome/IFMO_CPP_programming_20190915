1c66f37538334a66ca9cd001577dad001a365a3d
Marygith <50752864+Marygith@users.noreply.github.com>
2019-11-25 19:56:53 +0300

    fixed points

--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -22,11 +22,28 @@
 //2 3
 
 #include "t03_points.h"
-#include <iostream>
-
+#include <iostream>#include <iostream>
+#include<set>
+#include<map>
+#include <string>
 
 using namespace std;
 
 int t03_points() {
-    
+    multiset <double> dist;
+map <double,string> points;
+int n, x, y;
+double d;
+string a;
+cin >> n;
+getline(cin, a);
+for(int i = 0; i < n; n++) {
+getline(cin, a);
+x = stoi(a.substr(0, a.find(' ')));
+y = stoi(a.substr(a.find(' ') + 1, a.size()));
+d = (x*x + y*y)^0,5;
+dist.insert(d);
+points[d] = a;
+}
+for(auto i : dist) {cout << points[i];}
 }
