0fcb6fdaf6c8e833b930889d87521ac07bdba0e8
Marygith <50752864+Marygith@users.noreply.github.com>
2019-11-25 23:09:29 +0300

    fixed students

--- src/main/cpp/t04_students.cpp
+++ src/main/cpp/t04_students.cpp
@@ -29,10 +29,37 @@
 
 #include "t04_students.h"
 #include <iostream>
-
+#include<string>
+#include<set>
+#include<map>
+#include<vector>
 using namespace std;
 
 int t04_students() {
-    
+    int n;
+double numb;
+vector <string> v;
+set <double> m;
+map <double,vector<string> > stmark;
+string student, a, marks;
+cin >> n;
+cin >> student;
+for(int i = 0; i < n; i++)
+{getline(cin, student);
+a = student.substr(0, student.find(' ') + 1);
+student.erase(0, student.find(' ') + 1);
+a += student.substr(0, student.find(' '));
+student.erase(0, student.find(' ') + 1);
+numb = (stoi(&student[0]) + stoi(&student[2]) + stoi(&student[4]))/3;
+
+if(m.count(numb)!= 0){stmark[numb].push_back(a);}
+else{
+m.insert(numb);
+v.push_back(a);
+stmark[numb] = v;
+v.pop_back();   
+    }
+}
+for(auto i : m) {for(auto k : stmark[i]) {cout << k << endl;}}
 }
 
