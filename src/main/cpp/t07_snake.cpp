f5b4d948c52ab4dee72869ae8aebb6af6fa2463c
deadinsidezxc <lordsymon99@mail.ru>
2019-11-26 16:40:53 +0300

    BlessRNG v1.3

--- src/main/cpp/t07_snake.cpp
+++ src/main/cpp/t07_snake.cpp
@@ -31,14 +31,14 @@ int t07_snake() {
             for (int j = 0; j < m; j++) {
                 counter++;
                 a[i][j] = counter;
-                cout << preccision(6) << a[i][j];
+                cout << precission(6) << a[i][j];
             };
         }
         else {
             for (int j = m-1; j >= 0; j--) {
                 counter++;
                 a[i][j] = counter;
-                cout << preccision(4) << a[i][j];
+                cout << precission(4) << a[i][j];
             };
         };
     };
