1af4ac572aaee65db40b698f788ad1aa0b17c5ce
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-11-28 21:17:19 +0300

    Update t04_both2.cpp

--- src/main/cpp/t04_both2.cpp
+++ src/main/cpp/t04_both2.cpp
@@ -23,9 +23,26 @@
 
 #include "t04_both2.h"
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 int t04_both2() {
-
+  int N = 0, n = 0, M = 0;
+    multiset <int> qwe;
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        cin >> n;
+        qwe.insert(n);
+    }
+    cin >> M;
+    for (int i = 0; i < M; i++){
+        cin >> n;
+        qwe.insert(n);
+    }
+    for (multiset<int>::iterator WE = qwe.begin(); WE != qwe.end(); ++WE)
+    if(qwe.count(*WE) > 1){
+        cout <<*WE << " ";
+        WE = ++qwe.find(*WE);
+    }
 }
