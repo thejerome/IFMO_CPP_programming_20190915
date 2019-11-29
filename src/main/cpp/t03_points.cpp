ae47b8cb9ec7ff3c49416b1df4a4b56f85222b7c
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-11-29 17:44:06 +0300

    Update t03_points.cpp

--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -26,7 +26,35 @@
 
 
 using namespace std;
+struct junk{
+    int TTR[2];
+    double rast;
+};
+double distance (double x, double y){
+    double rast = sqrt (x * x + y * y);
+    return rast;
+}
 
 int t03_points() {
-    
+    int N;
+    cin >> N;
+    junk a[N + 1];
+    for(int i = 0; i < N; i++){
+        cin >> a[i].TTR[0];
+        cin >> a[i].TTR[1];
+        a[i].rast = distance(a[i].TTR[0], a[i].TTR[1]);
+    }
+    for(int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (a[i].rast > a[j].rast){
+                a[N] = a[i];
+                a[i] = a[j];
+                a[j] = a[N];
+            }
+        }
+    }
+    for (int i = 0; i < N; i++){
+        cout << a[i].TTR[0] << " ";
+        cout << a[i].TTR[1] << " ";
+    }
 }
