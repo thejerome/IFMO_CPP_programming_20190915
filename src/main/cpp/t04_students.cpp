b4d30a9ce067562aa04342eaa1782287e7160643
Danil Zaytsev <55970698+mamapomogi@users.noreply.github.com>
2019-11-29 18:03:28 +0500

    Update t04_students.cpp

--- src/main/cpp/t04_students.cpp
+++ src/main/cpp/t04_students.cpp
@@ -29,10 +29,38 @@
 
 #include "t04_students.h"
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <string>
 using namespace std;
+struct student {
+    string surname;
+    string name;
+    int rating;
+};
 
-int t04_students() {
-    
+bool cmp(student a, student b) {
+    return a.rating > b.rating;
 }
-
+int t04_students() {
+ int n;
+    cin >> n;
+    vector <student> students(n);
+    for (int i = 0; i < n; i++) {
+        int s_rating, s_mark1, s_mark2, s_mark3;
+        string s_surname, s_name;
+        cin >> s_surname >> s_name >> s_mark1 >> s_mark2 >> s_mark3;
+                
+        student student_temp;
+        student_temp.surname = s_surname;
+        student_temp.name = s_name;
+        student_temp.rating = s_mark1 + s_mark2 + s_mark3;
+        
+        students[i] = student_temp;
+    }
+    stable_sort(students.begin(), students.end(), cmp);
+    for (auto now : students) {
+        cout << now.surname << " " << now.name << endl;
+    }
+    return 0;
+}   
