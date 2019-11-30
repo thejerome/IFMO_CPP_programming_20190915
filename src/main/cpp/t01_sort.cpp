ff03c57976f384d869a2387f9bf15404014f1ee8
Sleepy2033 <55032506+Sleepy2033@users.noreply.github.com>
2019-11-30 20:52:36 +0300

    Update t01_sort.cpp

--- src/main/cpp/t01_sort.cpp
+++ src/main/cpp/t01_sort.cpp
@@ -24,5 +24,28 @@
 using namespace std;
 
 int t01_sort() {
-    
+     int *a;
+    int m;
+    cout<<"n = ";
+    cin>>m;
+    if (m<=0){
+        cerr<<"Invalid m"<<endl;
+        return 1;}
+    a=new int[m];
+    for(int i=0;i<m;i++){
+        cout<<"a["<<i<<"]=";
+        cin>>a[i];}
+    int x;
+    for (int i=0;i<m-1;i++) {
+        for (int k=0;k<m-i-1;k++) {
+            if (a[k]>a[k+1]) {
+                x=a[k];
+                a[k]=a[k+1];
+                a[k+1]=x;}
+        }
+    }
+    for (int i=0;i<m;i++){
+        cout<<a[i]<<" ";}
+    cout<<endl;
+    return 0; 
 }
