7c57504eab11a0ff6a409b9829b7afb34387159b
Veiden1 <56076058+Veiden1@users.noreply.github.com>
2019-12-01 15:15:29 +0300

    Update t01_unique.cpp

--- src/main/cpp/t01_unique.cpp
+++ src/main/cpp/t01_unique.cpp
@@ -19,9 +19,17 @@
 
 #include "t01_unique.h"
 #include <iostream>
-
+#include <set>
 using namespace std;
 
 int t01_unique() {
-    
+   int n, x, i;
+    cin >> n;
+    set <int> s;
+    for (i = 0; i < n; i++) {
+     cin >> x;
+     s.insert(x);  
+    }
+    cout << s.size();
+  return 0;   
 }
