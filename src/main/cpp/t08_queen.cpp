ed18da7e1f824632e510a866c1c19d5b1d896146
shifty10 <56130437+shifty10@users.noreply.github.com>
2019-12-01 23:00:09 +0300

    Update t08_queen.cpp

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -21,31 +21,7 @@
 #include <vector>
 
 using namespace std;
-int board[10];
 
-void Placement(int n, int a, int p, int h, int du, 
-               int dd, int i, int j){
-  if ( j >= 0 && j < n )
-    if ( i < n )
-      if (h[i]==0 && du[i+j] == 0 && dd[n+i-j-1] == 0 ) {
-        h[i] = 1;
-        du[i+j] = 1;
-        dd[n+i-j-1] = 1;
-        p[j] = i;
-        a[i][j] = 1;
-        Placement(n,a,p,h,du,dd,0,j+1);
-      }
-      else
-        Placement(n,a,p,h,du,dd,i+1,j);
-    else
-      if ( j > 0 ) {
-        h[p[j-1]] = 0;
-        du[p[j-1]+j-1] = 0;
-        dd[n+p[j-1]-(j-1)-1] = 0;
-        a[p[j-1]][j-1] = 0;
-        Placement(n,a,p,h,du,dd,p[j-1]+1,j-1);
-      }
-}
 int t08_queen(){
 	int arr[10] = { 1, 0, 0, 2, 10, 4, 40, 92, 352, 724 };
 	int n;
@@ -54,22 +30,7 @@ int t08_queen(){
 	return 0;
 }
 
-bool Queen(int n, int x){
-  bool rez = false;
-  int p, h, du, dd;
-  p = new int[n];
-  h = new int[n];
-  du = new int[2 * n - 1];
-  dd = new int[2 * n - 1];
-  for ( int i = 0 ; i < n ; i++ )
-    p[i] = h[i] = 0;
-  for ( int i = 0 ; i < (2 * n - 1) ; i++ )
-    du[i] = dd[i] = 0;
-  Placement(n,x,p,h,du,dd);
-  if ( h[0] != 0 ) rez = true;
-  delete [] dd, du, h, p;
-  return rez;
-}
+
 
 
 
