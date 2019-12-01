d834458c2af393cc88bb9cdb0370f8222d8d3213
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-12-01 15:06:54 +0300

    Update t01_min.cpp

--- src/main/cpp/t01_min.cpp
+++ src/main/cpp/t01_min.cpp
@@ -28,7 +28,7 @@ int min(int a, int b){
 }
 //function min4 
 int min4(int a,int b,int c,int d){
-    min(min(a, b), min(c, d));
+    return min(min(a, b), min(c, d));
 }
 int t01_min() {
     int q, w, e, r;
