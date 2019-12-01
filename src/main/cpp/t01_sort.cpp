f03af08c5024a585dac5ff3ee780d28b10b9d1be
malkyatmuk1 <tanya.naidenova@abv.bg>
2019-12-01 15:21:52 -0500

    add all tasks

--- src/main/cpp/t01_sort.cpp
+++ src/main/cpp/t01_sort.cpp
@@ -2,7 +2,7 @@
 //
 //Входные данные
 //
-//Первая строка входных данных содержит количество элементов в массиве N ≤ 10^5. 
+//Первая строка входных данных содержит количество элементов в массиве N ≤ 10^5.
 // Далее идет N целых чисел, не превосходящих по абсолютной величине 10^9.
 //
 //Выходные данные
@@ -22,7 +22,41 @@
 
 
 using namespace std;
+void sort(int arr[],int l,int r) {
+    int i = l, j =r;
+    int swap;
+    int center=arr[(l+r)/2];
+    while (i<=j) {
+        while (arr[i]<center)
+        i++;
+        while (arr[j]>center)
+        j--;
+        if (i<=j) {
+            swap=arr[i];
+            arr[i]=arr[j];
+            arr[j]=swap;
+            i++;
+            j--;
+        }
+    }
+    if (l<j)
+    sort(arr,l,j);
+    if (i<r)
+    sort(arr,i,r);
 
+}
 int t01_sort() {
-    
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+      cin>>arr[i];
+    }
+    sort(arr,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+      if(i!=n-1)cout<<arr[i]<<" ";
+      else cout<<arr[i];
+    }
 }
--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -2,9 +2,9 @@
 //
 //Входные данные
 //
-//Программа получает на вход набор точек на плоскости. 
+//Программа получает на вход набор точек на плоскости.
 // Сначала задано количество точек n, затем идет последовательность из n строк,
-// каждая из которых содержит два числа: координаты точки. 
+// каждая из которых содержит два числа: координаты точки.
 // Величина n не превосходит 100, все исходные координаты – целые числа, не превосходящие 10^3.
 //
 //Выходные данные
@@ -23,10 +23,42 @@
 
 #include "t03_points.h"
 #include <iostream>
-
+#include<cmath>
 
 using namespace std;
+struct point{
+  int x;
+  int y;
 
+};
+double rast(point p)
+{
+  return sqrt(pow(0-p.x,2)+pow(0-p.y,2));
+}
+void sort(point arr[], int n)
+{
+  point swap;
+  for(int i=0;n >i;i++)
+    for(int j=n-1;j>i;j--)
+       if(rast(arr[j-1]) >rast(arr[j]))
+        {
+           swap=arr[j-1];
+           arr[j-1]=arr[j];
+           arr[j]=swap;
+        }
+}
 int t03_points() {
-    
+int n;
+cin>>n;
+point arr[n];
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i].x;
+    cin>>arr[i].y;
+}
+sort(arr,n);
+for(int i=0;i<n;i++)
+{
+  cout<<arr[i].x<<" "<<arr[i].y<<endl;
+}
 }
