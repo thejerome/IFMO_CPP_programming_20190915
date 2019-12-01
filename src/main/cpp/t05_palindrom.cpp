a44f8b9ba299ec5f17e83fa66f05c4cfd6308837
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-12-01 17:02:06 +0300

    Update t05_palindrom.cpp

--- src/main/cpp/t05_palindrom.cpp
+++ src/main/cpp/t05_palindrom.cpp
@@ -18,10 +18,21 @@
 
 #include "t05_palindrom.h"
 #include <iostream>
-
+#include <string>
 
 using namespace std;
 
 int t05_palindrom() {
-
+  string s;
+    bool k=1;
+    getline(cin,s);
+    for (int i = 0;i < s.length()/2;i++){
+        if ((k) && (s[i] != s[s.length()-i-1])){
+            cout << "no";
+            k=0;
+        }
+    }
+    if(k){
+        cout << "yes";
+    }
 }
