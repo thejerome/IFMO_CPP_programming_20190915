030b5ec1b52da917266f41abaf7c9d299262565f
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2019-12-01 14:52:14 +0300

    Update t08_queen.cpp

--- src/main/cpp/t08_queen.cpp
+++ src/main/cpp/t08_queen.cpp
@@ -22,7 +22,29 @@
 
 using namespace std;
 
-int t08_queen(){
+int doska[10];
+bool check(int q, int w, int e) {
+    if (e == q) return true;
+    else return doska[e] != w && (q - e) != (w - doska[e]) && (q - e) != (doska[e] - w) && check(q, w, e + 1);
+}
 
+int ferz(int n, int i, int j) {
+    if (i == n) return 1;
+    else {
+        if (j < n) {
+            int Q = 0;
+            if (check(i, j, 0)) {
+                doska[i] = j;
+                Q = ferz(n, i + 1, 0);
+            }
+            return Q + ferz(n, i, j + 1);
+        }
+        else return 0;
+    }
+}
+int t08_queen(){
+    int N;
+    cin >> N;
+    cout << ferz(N, 0, 0);
     return 0;
 }
