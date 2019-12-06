93a546021dcdbb9fcc581aa0b41af892098964f5
Yurikyakyt <55650447+Yurikyakyt@users.noreply.github.com>
2019-12-06 11:15:34 +0300

    Update t01_sort.cpp

--- src/main/cpp/t01_sort.cpp
+++ src/main/cpp/t01_sort.cpp
@@ -24,5 +24,14 @@
 using namespace std;
 
 int t01_sort() {
-    
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    sort(a.begin(),a.end());
+    for (int i=0;i<n;i++){
+        cout << a[i] << ' ';
+    }    
 }
