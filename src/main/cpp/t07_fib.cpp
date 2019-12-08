e4bd6542da6e6c0b8874512f74a5f5599d5600cb
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-12-08 19:15:53 +0300

    Update t07

--- src/main/cpp/t07_fib.cpp
+++ src/main/cpp/t07_fib.cpp
@@ -17,9 +17,18 @@
 
 using namespace std;
 
-// function fib
+int fib(int n) {
+    if (n==0) {
+        return 0;
+    }
+    if (n==1) {
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
 
 int t07_fib()
-{
-    return 0;
+    int n;
+    cin >> n;
+    cout << fib(n);
 }
