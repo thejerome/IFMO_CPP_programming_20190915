7b20144e8ef327d8c21d4326079c52bf0e91f693
chromyy <55399522+chromyy@users.noreply.github.com>
2019-12-09 04:55:03 +0300

    Update t06_cinema.cpp

--- src/main/cpp/t06_cinema.cpp
+++ src/main/cpp/t06_cinema.cpp
@@ -29,6 +29,31 @@
 using namespace std;
 
 int t06_cinema() {
-    
+    	int n, num, m, cou, row;
+	cin >> n >> m;
+	int arr[n][m];
+	
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			cin >> arr[i][j];
+		}
+	}
+	
+	cin >> num;
+	for (int i = 0; i < n; i++){
+        for (int j = 1; j < m; j++){
+            if (arr[i][j] == 0){
+            	cou++;
+            	if (cou == num){
+            		row = 1 + i;
+            		break;
+            	}
+            }
+            else {
+            	cou = 0;
+            }
+        }
+	}
+	cout << row;
     return 0;
 }
