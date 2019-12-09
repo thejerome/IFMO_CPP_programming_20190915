7e18c82f49ca2e2b2ca8611b82cd200bcb948f11
Mark <m.gurianov@mail.ru>
2019-12-10 00:54:04 +0300

    Гурьянов Марк P3120 task-10

--- src/main/cpp/t01_sort.cpp
+++ src/main/cpp/t01_sort.cpp
@@ -19,10 +19,18 @@
 
 #include "t01_sort.h"
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int t01_sort() {
-    
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0 ; i < n ; i ++)
+        cin >> vec[i];
+    sort(vec.begin() , vec.end());
+    for (int now : vec)
+        cout << now << " ";
 }
