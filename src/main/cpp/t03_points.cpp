884267e9379dcceb3b968b5967c9d31d60ab22ce
Yurikyakyt <55650447+Yurikyakyt@users.noreply.github.com>
2019-12-12 12:53:41 +0300

    Update t03_points.cpp

--- src/main/cpp/t03_points.cpp
+++ src/main/cpp/t03_points.cpp
@@ -23,10 +23,33 @@
 
 #include "t03_points.h"
 #include <iostream>
-
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-
+struct dot{
+        int x,y;
+        double l;
+    };
+bool c(dot a, dot b){
+    return(a.l>b.l);
+}
 int t03_points() {
-    
+    int n;
+    cin >> n;
+    vector <dot> a(n);
+    for (int i = 0;i<n;i++){
+        int x,y;
+        cin >> x >> y;
+        dot d;
+        d.x = x;
+        d.y = y;
+        d.l = sqrt(x^2+y^2);
+        a[i] = d;
+    }
+    sort(a.begin(),a.end(),c);
+    for (int i=0;i<n;i++){
+        cout << a[i].x << ' ' << a[i].y << endl;
+    }    
 }
