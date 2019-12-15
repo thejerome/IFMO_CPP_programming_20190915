ea5c0b902fa16052c50a12bd8ab9bbf0a01d23c1
Sasha Mikhailova <55459169+MikhSaas@users.noreply.github.com>
2019-12-16 01:11:56 +0300

    Update t06_homework.cpp

--- src/main/cpp/t06_homework.cpp
+++ src/main/cpp/t06_homework.cpp
@@ -107,7 +107,7 @@ int t06_homework() {
         d1.insert(a);
         d[a]=a2;
     }
-    for (int i=0;i<5;i++) {
+    for (int i=0;i<10;i++) {
         cin>>a;
         a2="";
         for (int j=0;j<a.length();j++){
