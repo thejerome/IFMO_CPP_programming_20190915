18f0bbe302ae60375c8e0ceace663e7ac870a9f4
Kirill Slobodenyuk <kirill_slobodenyuk@mail.ru>
2019-12-20 21:27:01 +0300

    awesome

--- src/main/cpp/t06_quad.cpp
+++ src/main/cpp/t06_quad.cpp
@@ -21,5 +21,17 @@
 using namespace std;
 
 int t06_quad() {
-
+    double a, b, c, D;
+    cin >> a >> b >> c;
+    D = b * b - 4*a*c;
+    if (D > 0){
+        double x1 = (-b - sqrt(D)) / (2*a);
+        double x2 = (-b + sqrt(D)) / (2*a);
+        cout << x1 << " " << x2;
+    }
+    else if (D == 0) {
+        double x0 = (-b) / (2*a);
+        cout << x0;    
+    }
+    return 0;
 };
