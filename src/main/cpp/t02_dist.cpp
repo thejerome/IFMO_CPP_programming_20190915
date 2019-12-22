d225b2c6f38997c70330350079d99bee7d9ce84c
Chamyys <42144057+Chamyys@users.noreply.github.com>
2019-12-23 01:41:58 +0300

    Update t02_dist.cpp

--- src/main/cpp/t02_dist.cpp
+++ src/main/cpp/t02_dist.cpp
@@ -22,7 +22,7 @@
 using namespace std;
 
 float distance(float x1, float x2, float y1, float y2) {
-		return sqrt((pow((x2 - x1), 2) + (pow((y2 - y1), 2));
+		return sqrt(pow((x2 - x1), 2) + (pow((y2 - y1), 2)));
 	}
 
 int t02_dist() {
