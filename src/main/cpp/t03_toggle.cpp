9daddb06fcac97d2a9ee8e56a5ad2170fda990fb
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-12-24 19:41:35 +0300

    Update t03

--- src/main/cpp/t03_toggle.cpp
+++ src/main/cpp/t03_toggle.cpp
@@ -24,13 +24,16 @@
 using namespace std;
 
 int t03_toggle() {
-    char a;
+      char a;
     cin >> a;
     if ( a >= 'a' and a <= 'z' ) {
         cout << char (a-32) ;
+        return 0;
     }
     if ( a >= 'A' and a <= 'Z') {
         cout << char (a+32) ;
+        return 0;
+        
     }
     else cout << a;
     return 0;
