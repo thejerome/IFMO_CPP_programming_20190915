b33b3f07a4667a4683e3d3252c9c6049ee59711f
DrakeArc <56124913+DrakeArc@users.noreply.github.com>
2019-12-24 17:44:21 +0300

    Update t02

--- src/main/cpp/t02_star.cpp
+++ src/main/cpp/t02_star.cpp
@@ -29,20 +29,20 @@ cin >> n ;
 char a[n][n] ;
 for( int i = 0; i < n ; i++) {
     for( int j = 0 ; j < n ; j++) {
-            a[i][j]=".";
+            a[i][j] = '.';
     }
 }
 for( int i = 0; i < n; i++) {
     for(int j = 0 ; j < n; j++) {
-       if((i==(n-1)/2) | (j==(n-1)/2) | (i==j) | (n==j+i) {
-           a[i][j]="*";
+       if((i==(n-1)/2) | (j==(n-1)/2) | (i==j) | (n==j+i)) {
+           a[i][j] = '*';
         }
     }
 }
 for (int i = 0; i < n; i++) {
     for(int j = 0; j < n ; j++) {
-        cout << a[i][j] << " ";
+        cout << a[i][j] << ' ';
     }
     cout << endl;
-}
+ }
 }
