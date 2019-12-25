afb97806e142f0a66b85f9b2984b631e4e342230
nagizune <neolliss@gmail.com>
2019-12-25 19:50:32 +0300

    uploaded

--- src/main/cpp/t06_homework.cpp
+++ src/main/cpp/t06_homework.cpp
@@ -119,47 +119,47 @@ int t06_homework() {
 	std::stringstream ss(t);
 	std:: string word;
 	while (ss >> word) {
-		std::string t = word;
-		std::transform(t.begin(),
-			       t.end(),
-			       t.begin(),
-			       ::toupper);
-		if (dictionary.find(t) != dictionary.end()) {
-			myIt lb;
-			myIt ub;
-			lb = dictionary.lower_bound(t);
-			ub = dictionary.upper_bound(t);
+		    std::string t = word;
+		    std::transform(t.begin(),
+			            t.end(),
+			            t.begin(),
+			            ::toupper);
+		    if (dictionary.find(t) != dictionary.end()) {
+			        myIt lb;
+			        myIt ub;
+			        lb = dictionary.lower_bound(t);
+			        ub = dictionary.upper_bound(t);
 
-			bool loc = true;
-			for (myIt it = lb; it != ub; it++) {
-				if ( (*it).second == word ) {
-					loc = false;
-					break;
-				}
-			}
-			if (loc == true) { count++; }
-			continue;
-		}
+			    bool loc = true;
+			    for (myIt it = lb; it != ub; it++) {
+				        if ( (*it).second == word ) {
+					            loc = false;
+					            break;
+				        }
+			    }
+			    if (loc == true) { count++; }
+			    continue;
+		    }
 
 
-		if (Udarenie(&word) == true) {
-			continue;
-		} else {
-			count++;
-		}
+		    if (Udarenie(&word) == true) {
+			    continue;
+		    } else {
+			        count++;
+		    }
 	}
 	std::cout << count << EL;
 }
 
 bool Udarenie(const string *w)
 {
-	bool sf = false;
-	for (size_t i = 0; i < w->size(); i++ ) {
-		if ( ((*w)[i] >= 'A') && ((*w)[i] <= 'Z') ) {
-			if (sf == true) { return false; }
-			sf = true;
-		}
-	}
-	return sf;
+	    bool sf = false;
+	    for (size_t i = 0; i < w->size(); i++ ) {
+		        if ( ((*w)[i] >= 'A') && ((*w)[i] <= 'Z') ) {
+			            if (sf == true) { return false; }
+			            sf = true;
+		        }
+	    }
+	    return sf;
 }
 
