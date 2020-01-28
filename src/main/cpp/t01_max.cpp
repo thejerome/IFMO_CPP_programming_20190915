3ac274aa7c183a942b35849efb6fe25e7eceb3b0
Kirill Slobodenyuk <kirill_slobodenyuk@mail.ru>
2020-01-28 21:56:57 +0300

    first step

--- src/main/cpp/t01_max.cpp
+++ src/main/cpp/t01_max.cpp
@@ -27,6 +27,25 @@
 using namespace std;
 
 int t01_max() {
-    
+    int n, m;
+    cin >> n, m;
+    int arr[100][100];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int max = arr[0][0], max_i = 0, max_j = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if(arr[i][j] > max)
+            {
+                max = arr[i][j];
+                max_i = i;
+                max_j =j;
+            }
+        }
+    }
+    cout << max_i << " " << max_j;
     return 0;
 }
