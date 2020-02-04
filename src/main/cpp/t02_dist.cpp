83e01171b2196c2b1e49093665fe633356a179b8
Kirill Slobodenyuk <kirill_slobodenyuk@mail.ru>
2020-02-04 18:29:17 +0300

    awesome

--- src/main/cpp/t02_dist.cpp
+++ src/main/cpp/t02_dist.cpp
@@ -22,8 +22,14 @@
 using namespace std;
 
 //function distance
-
+double distance(double x1, double y1, double x2, double y2) {
+	double a = abs(x1 - x2);
+	double b = abs(y1 - y2);
+	return sqrt(a * a + b * b);
+}
 int t02_dist() {
-
+	double x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	cout << distance(x1, y1, x2, y2);
     return 0;
 }
