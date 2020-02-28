75fee92d7cb585ca0ba2bdb553f1d2b0c87dfc74
vasyakhan <vasyakhan01@gmail.com>
2020-02-28 18:20:17 +0300

    task 4

--- src/main/cpp/t07_quad2.cpp
+++ src/main/cpp/t07_quad2.cpp
@@ -29,6 +29,9 @@ if (a == 0 && b == 0 && c == 0) {
 if(a == 0 && b == 0 && c != 0){
     cout << 0;
     return 0;};
+if(a == 0){
+    cout << 1 << " " << (-c / b);
+    return 0;};
 d = (b*b - 4*a*c);
 if (d < 0) cout << 0;
 if (d > 0) 
